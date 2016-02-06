7f6ef4e02ff82e7236236d7721ed7520f906a640
Youssef <youssefhad2@gmail.com>
2016-02-06 21:26:55 +0000

    update root to make it initialize it self

--- Http/Root.php
+++ Http/Root.php
@@ -8,9 +8,26 @@ use Fiesta\Kernel\Foundation\Application;
 */
 class Root
 {
-	public static $css=Application::$root."app/resources/css/";
-	public static $js=Application::$root."app/resources/js/";
-	public static $resource=Application::$root."app/resources/";
-	public static $images=Application::$root."app/resources/images/";
-	public static $scripts=Application::$root."app/scripts/";
+	public static $css;
+	public static $js;
+	public static $resource;
+	public static $images;
+	public static $scripts;
+
+	/**
+	 * initial function of class
+	 */
+	public static function ini()
+	{
+		self::$css      = Application::$root."app/resources/css/";
+		self::$js       = Application::$root."app/resources/js/";
+		self::$resource = Application::$root."app/resources/";
+		self::$images   = Application::$root."app/resources/images/";
+		self::$scripts  = Application::$root."app/scripts/";
+	}
 }
+
+/**
+ * calling initial function of class
+ */
+Root::ini()
