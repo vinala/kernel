aadd73a61a9e996bab96962f4a26618da196a0a0
Youssef <youssefhad2@gmail.com>
2016-02-20 21:27:21 +0000

    call function to set alias for Intro class

--- Foundation/Connector.php
+++ Foundation/Connector.php
@@ -5,6 +5,7 @@ namespace Fiesta\Kernel\Foundation;
 use Fiesta\Kernel\Config\Config;
 use Fiesta\Kernel\Logging\Log;
 use Fiesta\Kernel\Logging\Handler;
+use Fiesta\Kernel\Intro\Intro;
 
 /**
 * Connector class to call framework core files
@@ -507,5 +508,6 @@ class Connector
 	public static function intro()
 	{
 		self::need(self::$path.'Access/Intro.php');
+		Intro::alias();
 	}
 }
