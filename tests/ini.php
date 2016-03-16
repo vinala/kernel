035f961b8d7f1f7ae3efe91cc6deb1c49d87c75c
Youssef <youssefhad2@gmail.com>
2016-03-16 22:26:40 +0000

    show all folders of current folder  if ini.php

--- tests/ini.php
+++ tests/ini.php
@@ -1,6 +1,8 @@
 <?php
+echo var_dump(glob("*"));
 
 require __DIR__."/../../autoload.php";
+
 
 // Fiesta\Kernel\Foundation\Application::run("../",true,true,"Connector.php");
 Fiesta\Kernel\Foundation\Application::test();
