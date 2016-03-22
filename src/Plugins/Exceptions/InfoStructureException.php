c1112689781d4bdbd6dcd5c2ab35333508a75d42
Youssef <youssefhad2@gmail.com>
2016-03-22 23:09:45 +0000

    new InfoStructureException for syntax error in JSON

---
+++ src/Plugins/Exceptions/InfoStructureException.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace Fiesta\Kernel\Plugins\Exception;
+
+/**
+* Directory not fount exception
+*/
+class InfoStructureException extends \Exception
+{
+	protected $message;
+	//
+	function __construct($plugin) 
+	{
+		$this->message=".info file structure '$plugin' is not identical to the framework";
+	}
+}
