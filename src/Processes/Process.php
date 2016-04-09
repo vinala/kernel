06d4fbc3719c8a8321aa019d09c2d1fe547ce7d1
Youssef <youssefhad2@gmail.com>
2016-04-09 17:12:22 +0100

    new classes for panel Processes to use in console

---
+++ src/Processes/Process.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace Fiesta\Kernel\Process;
+
+/**
+* Language class
+*/
+class Process
+{
+	const root = "";
+}
---
+++ src/Processes/Translator.php
@@ -0,0 +1,45 @@
+<?php 
+
+namespace Fiesta\Kernel\Process;
+
+use Fiesta\Kernel\Process\Process;
+
+/**
+* Language class
+*/
+class Translator
+{
+	public static function createDir($name)
+	{
+		$root = Process::root;
+		//
+		if( ! file_exists($root."app/lang/".$name))
+		{
+			mkdir ($root."app/lang/".$name);
+			return true;
+		}
+		else return false;
+	}
+
+	public static function createFile($dirName , $fileName)
+	{
+		$root = Process::root;
+		//
+		if(!file_exists($root."app/lang/$dirName/$fileName.php"))
+			{
+				$myfile = fopen($root."app/lang/$dirName/$fileName.php", "w");
+				$txt = self::set();
+				//
+				fwrite($myfile, $txt);
+				fclose($myfile);
+				//
+				return true;
+			}
+			else return false;
+	}
+
+	public static function set()
+	{
+		return "<?php\n\nreturn array(\n\t'var_lan_name_1' => 'var_lang_value_1',\n\t'var_lan_name_2' => 'var_lang_value_2'\n);";
+	}
+}
