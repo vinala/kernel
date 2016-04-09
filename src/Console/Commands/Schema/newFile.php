baf38129a0d27ed1063537ff7a8b876974be7a3f
Youssef <youssefhad2@gmail.com>
2016-04-10 00:14:41 +0100

    add new schema file command

---
+++ src/Console/Commands/Schema/newFile.php
@@ -0,0 +1,41 @@
+<?php 
+
+namespace Fiesta\Kernel\Console\Command\Schema;
+
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Fiesta\Kernel\Console\Console;
+use Fiesta\Kernel\Process\Migrations;
+
+class NewSchemaCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('schema:new')
+            ->setDescription('make new schema file')
+            ->addArgument( 'name', InputArgument::REQUIRED, 'what\'s the name of the schema?');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name = $input->getArgument('name');
+        //
+        $process = Migrations::add($name);
+        //
+        $msg = self::message($process);
+        //
+        $output->writeln($msg);
+    }
+
+    protected static function message($process)
+    {
+        if($process) return Console::setMessage("The schema is created" , Console::ok );
+        //
+        else return Console::setMessage("The schema is already existe" , Console::err );
+    }
+}
