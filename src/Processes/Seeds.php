2c3e0bd0f8bd7910deee018e2cc2059d36fb5e10
Youssef <youssefhad2@gmail.com>
2016-04-10 12:36:53 +0100

    add process for seeds

---
+++ src/Processes/Seeds.php
@@ -0,0 +1,49 @@
+<?php 
+
+namespace Fiesta\Kernel\Process;
+
+use Fiesta\Kernel\Database\Seeder;
+
+/**
+* Seeds class
+*/
+class Seeds
+{
+
+	public static function exec()
+	{
+		return Seeder::ini();
+	}
+
+	public static function add($name,$table)
+	{
+		$nom=$name;
+		$Root = Process::root;
+		//
+		if(!file_exists($Root."app/seeds/$nom.php"))
+		{
+		 	$myfile = fopen($Root."app/seeds/$nom.php", "w");
+			$txt = self::set($nom,$table);
+			//
+			fwrite($myfile, $txt);
+			fclose($myfile);
+			//
+			return true;
+		}
+		else return false;
+	}
+
+	public static function set($nom,$table)
+	{
+		$txt = "<?php\n\nuse Fiesta\Kernel\Database\Seeder;\n\n";
+		$txt.="/**\n* class de seeder $nom\n*/\n\nclass $nom extends Seeder\n{\n";
+
+		//datatable name
+		$txt.="\t/*\n\t* Name of DataTable\n\t*/\n\tpublic ".'$table="'.$table.'";'."\n\n";
+
+			//run
+		$txt.="\t/*\n\t* Run the Database Seeder\n\t*/\n\tpublic function run()\n\t{\n\t\t".'$dataTable = array();'."\n\t\t//\n\t\t".'$dataTable[] = array(/* Data Fields */);'."\n\t\t//\n\t\t".'return Schema::table($this->table)->insert($dataTable);'."\n\t}\n}";
+
+		return $txt;
+	}
+}
