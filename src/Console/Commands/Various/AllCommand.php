7482313b73f0615cea3cb556934445b6976d0796
Youssef <youssefhad2@gmail.com>
2016-04-19 20:26:19 +0100

    update all command class

--- src/Console/Commands/Various/AllCommand.php
+++ src/Console/Commands/Various/AllCommand.php
@@ -10,6 +10,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Pikia\Kernel\Console\Console;
 use Pikia\Kernel\Config\Config;
+use Pikia\Kernel\Objects\Strings;
 
 
 
@@ -33,54 +34,194 @@ class AllCommand extends Command
     {
         $msg = $this->all();
         //
-        // $msg = self::message($process);
-        //
         $output->writeln($msg);
     }
 
 
     /**
-     * Format the message to show
-    */
-    protected static function message($process)
+     * Complete with specific char
+     */
+    protected function completeCom($key , $max)
     {
-        if($process == 1) return Console::setMessage("The view is created" , Console::lst );
-        else if($process == 2) return Console::setMessage("The view is already existe" , Console::err );
-            //"Le fichier deja existe"
-        else if($process == 3) return Console::setMessage("The view is already existe" , Console::err );
+        $char = ".";        
+        //
+        $command = $this->cmnds();
+        $command = $command[$key];
+        $lenght = Strings::lenght($command);
+        //
+        $differnce = $max - $lenght;
+        //
+        $result = $command;
+        //
+        for ($i=0; $i <= $differnce+3 ; $i++) 
+            $result .= $char;
+        //
+        return $result;
+    }
+
+    protected function completeDoc($key , $max)
+    {
+        $char = ".";        
+        //
+        $docs = $this->docs($key);
+        $docs = $docs[$key];
+        $lenght = Strings::lenght($docs);
+        //
+        $differnce = $max - $lenght;
+        //
+        $result = $docs;
+        //
+        for ($i=0; $i <= $differnce+3 ; $i++) 
+            $result .= $char;
+        //
+        return $result;
     }
 
     /**
-     * Format the message to show
-    */
+     * Get the lenght of the longest command
+     */
+    protected function cmnMaxSize()
+    {
+        $cmd = self::cmnds();
+        //
+        $max = 0;
+        // die(var_dump($cmd));
+        //
+        foreach ($cmd as $key => $value) 
+        {
+            if(is_array($value)) die('ttt');
+            if(Strings::lenght($value) > $max) $max = Strings::lenght($value);
+        }
+        //
+        return $max;
+    }
+
+    /**
+     * Get the lenght of the longest command
+     */
+    protected function docMaxSize()
+    {
+        $doc = self::docs();
+        //
+        $max = 0;
+        // die(var_dump($doc));
+        //
+        foreach ($doc as $key => $value) 
+        {
+            if(is_array($value)) die('ttts');
+            if(Strings::lenght($value) > $max) $max = Strings::lenght($value);
+        }
+        //
+        return $max;
+    }
+
+
+
+    /**
+     * All Documentation  of commands
+     */
+    protected function docs()
+    {
+        return array(
+            'info' => "Get info about the framework", 
+            'all' => "All Pikia commands", 
+            'new_schema' => "Make new schema file", 
+            'exec_schema' => "Execute the last schema created", 
+            'rollback_schema' => "Rollback the last schema created", 
+            'dir_lang' => "Make new translator directory", 
+            'file_lang' => "Make new translator file", 
+            'new_link' => "New file for links", 
+            'new_model' => "New Model", 
+            'new_view' => "New View", 
+            'new_controller' => "New Controller", 
+            'new_seed' => "New Seeder", 
+            'exec_seed' => "Execute Seeder", 
+            'get_routes' => "Add new get route to Routes file", 
+            );
+    }
+
+    /**
+     * All commands
+     */
+    protected function cmnds()
+    {
+        return array(
+            'info' => "info", 
+            'all' => "all", 
+            'new_schema' => Config::get('console.new_schema'), 
+            'exec_schema' => Config::get('console.exec_schema'), 
+            'rollback_schema' => Config::get('console.rollback_schema'), 
+            'dir_lang' => Config::get('console.dir_lang'), 
+            'file_lang' => Config::get('console.file_lang'), 
+            'new_link' => Config::get('console.new_link'), 
+            'file_lang' => Config::get('console.file_lang'), 
+            'new_link' => Config::get('console.new_link'), 
+            'new_model' => Config::get('console.new_model'), 
+            'new_view' => Config::get('console.new_view'), 
+            'new_controller' => Config::get('console.new_controller'), 
+            'new_seed' => Config::get('console.new_seed'), 
+            'exec_seed' => Config::get('console.exec_seed'), 
+            'get_routes' => Config::get('console.get_routes'), 
+            );
+    }
+
+
+    protected function title($title)
+    {
+        return Console::setMessage("$title\n" , Console::lst );
+    }
+
+    protected function showParams($params)
+    {
+        return Console::setCyan("$params");
+    }
+
+    protected function row($key , $maxCommand , $maxDoc , $params = "")
+    {
+        $command = $this->completeCom($key , $maxCommand);
+        $doc = $this->completeDoc($key , $maxDoc);
+        //
+        return Console::setMessage(" ".$command.$doc.($params != "" ? "|  ".$this->showParams($params) : "")."\n" , Console::nn);
+    }
+
     protected function all()
     {
+        $maxCommand = $this->cmnMaxSize();
+        $maxDocs = $this->docMaxSize();
+        //
         $process = "\n";
         //
-        $process .=Console::setMessage(" info............Get info about the framework\n" , Console::nn );
-        $process .=Console::setMessage(" all.............All Pikia commands\n" , Console::nn );
-        $process .=Console::setMessage("schema\n" , Console::lst );
-        $process .=Console::setMessage(" :new............make new schema file...............|".Console::setCyan("  <name>\n") , Console::nn );
-        $process .=Console::setMessage(" :exec...........Execute the last schema created\n" , Console::nn );
-        $process .=Console::setMessage(" :rollback.......Rollback the last schema created\n" , Console::nn );
-        $process .=Console::setMessage("lang\n" , Console::lst );
-        $process .=Console::setMessage(" :new:dir........make new translator directory......|".Console::setCyan("  <name>\n") , Console::nn );
-        $process .=Console::setMessage(" :new:file.......make new translator file...........|".Console::setCyan("  <fileName> <dirName>\n") , Console::nn );
-        $process .=Console::setMessage("link\n" , Console::lst );
-        $process .=Console::setMessage(" :new............New file for links.................|".Console::setCyan("  <fileName>\n") , Console::nn );
-        $process .=Console::setMessage("model\n" , Console::lst );
-        $process .=Console::setMessage(" :new............New model..........................|".Console::setCyan("  <fileName> <className> <tableName>\n") , Console::nn );
-        $process .=Console::setMessage("view\n" , Console::lst );
-        $process .=Console::setMessage(" :new............New View...........................|".Console::setCyan("  <fileName>  --smarty\n") , Console::nn );
-        $process .=Console::setMessage("controller\n" , Console::lst );
-        $process .=Console::setMessage(" :new............New Controller.....................|".Console::setCyan("  <fileName> <className>  --addRoute\n") , Console::nn );
-        $process .=Console::setMessage("seed\n" , Console::lst );
-        $process .=Console::setMessage(" :new............New Seeder.........................|".Console::setCyan("  <name> <tableName>\n") , Console::nn );
-        $process .=Console::setMessage(" :exec...........Execute Seeder\n" , Console::nn );
-        $process .=Console::setMessage("routes\n" , Console::lst );
-        $process .=Console::setMessage(" :get............Add new get route to Routes file...|".Console::setCyan("  <route>\n") , Console::nn );
+        $process .= $this->row("info" , $maxCommand , $maxDocs );
+        $process .= $this->row("all" , $maxCommand , $maxDocs );
+        //
+        $process .= $this->title("schema");
+        $process .= $this->row("new_schema" , $maxCommand , $maxDocs , "<name>");
+        $process .= $this->row("exec_schema" , $maxCommand , $maxDocs );
+        $process .= $this->row("rollback_schema" , $maxCommand , $maxDocs );
+        //
+        $process .= $this->title("lang");
+        $process .= $this->row("dir_lang" , $maxCommand , $maxDocs , "<name>");
+        $process .= $this->row("file_lang" , $maxCommand , $maxDocs , "<fileName> <dirName>");
+        //
+        $process .= $this->title("link");
+        $process .= $this->row("new_link" , $maxCommand , $maxDocs , "<fileName>");
+        //
+        $process .= $this->title("model");
+        $process .= $this->row("new_model" , $maxCommand , $maxDocs , "<fileName> <className> <tableName>");
+        //
+        $process .= $this->title("view");
+        $process .= $this->row("new_view" , $maxCommand , $maxDocs , "<fileName>  --smarty");
+        //
+        $process .= $this->title("controller");
+        $process .= $this->row("new_controller" , $maxCommand , $maxDocs , "<fileName> <className>  --addRoute");
+        //
+        $process .= $this->title("seed");
+        $process .= $this->row("new_seed" , $maxCommand , $maxDocs , "<name> <tableName>");
+        $process .= $this->row("exec_seed" , $maxCommand , $maxDocs);
+        //
+        $process .= $this->title("routes");
+        $process .= $this->row("get_routes" , $maxCommand , $maxDocs , "<route>");
         //
         return $process;
-
     }
 }
