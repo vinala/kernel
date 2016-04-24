ac988c5cd26f492f3a92d6c3525ce71a369e187b
Youssef <youssefhad2@gmail.com>
2016-04-24 01:37:55 +0100

    add command process

---
+++ src/Processes/Command.php
@@ -0,0 +1,40 @@
+<?php 
+
+namespace Pikia\Kernel\Process;
+
+use Pikia\Kernel\Process\Process;
+
+/**
+* Controller class
+*/
+class Command
+{
+	public static function create($file,$command)
+	{
+		$Root = Process::root;
+		//
+		if(!file_exists($Root."app/console/commands/$file.php")){
+			$myfile = fopen($Root."app/console/commands/$file.php", "w");
+			$txt = self::set($file, $command);
+			fwrite($myfile, $txt);
+			fclose($myfile);
+			//
+			return true;
+		}
+		else return false;
+
+	}
+
+	public static function set($file, $command)
+	{
+		$txt = "<?php\n\nnamespace App\Console\Commands;\n\n";
+		$txt .= "use Pikia\Kernel\Console\Command\Commands;\n\n";
+		$txt.="\n\nclass $file extends Commands\n{\n\t";
+
+		$txt.="\n\t/**\n\t * The name and signature of the console command.\n\t *\n\t * @var string\n\t */\n\tprotected ".'$key = '."'$command';\n\n";
+		$txt.="\n\t/**\n\t * The console command description.\n\t *\n\t * @var string\n\t */\n\tprotected ".'$description = '."'say hello to the world';\n\n";
+		$txt.="\n\t/**\n\t * Execute the console command.\n\t *\n\t * @return mixed\n\t */\n\tpublic function handle()\n\t{\n\t\t ".'$this->write("What\'s up!"); '."\n\t}";
+		$txt.="\n}";
+		return $txt;
+	}
+}
