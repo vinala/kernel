32afb9664673536d46b5b957dd4c378aa017432e
Youssef <youssefhad2@gmail.com>
2016-04-29 23:41:32 +0100

    update documentation of generated command class

--- src/Processes/Command.php
+++ src/Processes/Command.php
@@ -31,7 +31,7 @@ class Command
 		$txt .= "use Pikia\Kernel\Console\Command\Commands;\n\n";
 		$txt.="\n\nclass $file extends Commands\n{\n\t";
 
-		$txt.="\n\t/**\n\t * The name and signature of the console command.\n\t *\n\t * @var string\n\t */\n\tprotected ".'$key = '."'$command';\n\n";
+		$txt.="\n\t/**\n\t * The key of the console command.\n\t *\n\t * @var string\n\t */\n\tprotected ".'$key = '."'$command';\n\n";
 		$txt.="\n\t/**\n\t * The console command description.\n\t *\n\t * @var string\n\t */\n\tprotected ".'$description = '."'say hello to the world';\n\n";
 		$txt.="\n\t/**\n\t * Execute the console command.\n\t *\n\t * @return mixed\n\t */\n\tpublic function handle()\n\t{\n\t\t ".'$this->write("What\'s up!"); '."\n\t}";
 		$txt.="\n}";
