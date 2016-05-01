a70b4d0c430eafe0997591c2782b14830d59f594
Youssef <youssefhad2@gmail.com>
2016-05-01 14:20:48 +0100

    class for output of Linux bash

---
+++ src/Console/bashOutput.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Pikia\Kernel\Console\Command;
+
+use Pikia\Kernel\Objects\Strings;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Question\Question;
+
+/**
+* 
+*/
+class bashOutput
+{
+	
+	/**
+	 * the console input
+	 * @var OutputInterface
+	 */
+	public $output;
+
+	function __construct(OutputInterface $output)
+	{
+		$this->output = $output;
+	}
+
+	/**
+     * to write in the console
+     */
+    public function line($text)
+    {
+        $this->output->writeln($text);
+    }
+
+    /**
+     * to write in the console
+     */
+    public function write($key)
+    {
+         $this->output->write($key);
+    }
+
+    /**
+     * to write text in green color in the console
+     */
+    public function info($text)
+    {
+        $this->line("<info>".$text."</info>");
+    }
+
+    /**
+     * to write text in yellow color in the console
+     */
+    public function comment($text)
+    {
+        $this->line("<comment>".$text."</comment>");
+    }
+
+    /**
+     * to write text in cyan color in the console
+     */
+    public function question($text)
+    {
+        $this->line("<question>".$text."</question>");
+    }
+
+    /**
+     * to write text in red color in the console
+     */
+    public function error($text)
+    {
+        $this->line("<error>".$text."</error>");
+    }
+
+}
