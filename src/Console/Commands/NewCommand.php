a7e36f8636702cfb6aafa64ed511c48f5cca3693
Youssef <youssefhad2@gmail.com>
2016-05-05 23:29:17 +0100

    update new command class

---
+++ src/Console/Commands/NewCommand.php
@@ -0,0 +1,67 @@
+<?php 
+
+namespace Pikia\Kernel\Console\Commands;
+
+
+use Pikia\Kernel\Config\Config;
+use Pikia\Kernel\Console\Command\Commands;
+use Pikia\Kernel\Process\Command;
+
+
+
+class NewCommand extends Commands
+{
+
+    /**
+     * The key of the console command.
+     *
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    public $description;
+
+    /**
+     * Configure the command
+     */ 
+    public function set()
+    {
+        $this->key = Config::get('console.new_command').' {file : what\'s the name of the file?} {--command=greeting : the command}';
+        $this->description = "New User Console Cammand";
+    }
+
+    /**
+     * Handle the command
+     */
+    public function handle()
+    {
+        $this->exec();
+    }
+
+    /**
+     * Execute the command
+     */
+    public function exec()
+    {
+        $file = $this->argument("file");
+        $command = $this->option("command");
+        //
+        $process = Command::create($file , $command);
+        //
+        $this->show($process);
+    }
+
+    /**
+     * Format the message to show
+    */
+    public function show($process)
+    {
+        if($process) $this->info("The command is created");
+        else $this->error("The command is already existe");
+    }
+}
