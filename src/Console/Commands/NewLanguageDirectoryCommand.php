031d5debade3a7984a3e87ec61992af55be72ec9
Youssef <youssefhad2@gmail.com>
2016-05-06 20:48:08 +0100

    update NewLanguageDirectoryCommand class

---
+++ src/Console/Commands/NewLanguageDirectoryCommand.php
@@ -0,0 +1,66 @@
+<?php 
+
+namespace Pikia\Kernel\Console\Commands;
+
+
+use Pikia\Kernel\Config\Config;
+use Pikia\Kernel\Console\Command\Commands;
+use Pikia\Kernel\Process\Translator;
+
+
+
+class NewLanguageDirectoryCommand extends Commands
+{
+
+    /**
+     * The key of the console command.
+     *
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    public $description;
+
+    /**
+     * Configure the command
+     */ 
+    public function set()
+    {
+        $this->key = Config::get('console.dir_lang')." {name : what's the name of the directory?}";
+        $this->description = "make new translator directory";
+    }
+
+    /**
+     * Handle the command
+     */
+    public function handle()
+    {
+        $this->exec();
+    }
+
+    /**
+     * Execute the command
+     */
+    public function exec()
+    {
+        $name = $this->argument("name");
+        //
+        $process = Translator::createDir($name);
+        //
+        $this->show($process);
+    }
+
+    /**
+     * Format the message to show
+    */
+    public function show($process)
+    {
+        if($process) $this->info("The translator directory is created");
+        else $this->error("The translator directory is already existe");
+    }
+}
