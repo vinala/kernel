b9e94c5e9f19b19b66d4e48619ae1a9383e29511
Youssef <youssefhad2@gmail.com>
2016-05-06 19:51:14 +0100

    update ExecSeedCommand class

---
+++ src/Console/Commands/ExecSeedCommand.php
@@ -0,0 +1,64 @@
+<?php 
+
+namespace Pikia\Kernel\Console\Commands;
+
+
+use Pikia\Kernel\Config\Config;
+use Pikia\Kernel\Console\Command\Commands;
+use Pikia\Kernel\Process\Seeds;
+
+
+
+class ExecSeedCommand extends Commands
+{
+
+    /**
+     * The key of the console command.
+     *
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    public $description;
+
+    /**
+     * Configure the command
+     */ 
+    public function set()
+    {
+        $this->key = Config::get('console.exec_seed');
+        $this->description = 'Execute Seeder';
+    }
+
+    /**
+     * Handle the command
+     */
+    public function handle()
+    {
+        $this->exec();
+    }
+
+    /**
+     * Execute the command
+     */
+    public function exec()
+    {
+        $process = Seeds::exec();
+        //
+        $this->show($process);
+    }
+
+    /**
+     * Format the message to show
+    */
+    public function show($process)
+    {
+        if($process) $this->info("The seeder is executed");
+        else $this->error("There is an error".Database::execErr());
+    }
+}
