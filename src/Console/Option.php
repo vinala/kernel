c1185dd04ea7447a700fbb17c8a96c86be893fd6
Youssef <youssefhad2@gmail.com>
2016-05-09 22:05:20 +0100

    add class for command option

---
+++ src/Console/Option.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Pikia\Kernel\Console;
+
+/**
+*  Argument
+*/
+class Option
+{
+	
+	/**
+	 * name of argument
+     *
+	 * @var OutputInterface
+	 */
+	public $name;
+
+    /**
+     * if the argument is optional
+     */
+    public $requirement;
+
+    /**
+     * description of argument
+     */
+    public $description;
+
+    /**
+     * default value
+     */
+    public $value;
+
+
+
+	function __construct($name, $requirement = null, $description = "", $value = null)
+	{
+		$this->name = $name;
+        $this->requirement = $requirement;
+        $this->description = $description;
+        $this->value = $value;
+	}
+}
