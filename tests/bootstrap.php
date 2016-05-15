a4d64ce69ffb30a2616aed36293104f1d0b58152
Youssef <youssefhad2@gmail.com>
2016-05-16 00:39:29 +0100

    test for root path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,12 @@
 
 
 // die(var_dump(glob(__DIR__.'/../../../*')));
-require __DIR__.'/../../../autoload.php';
+// require __DIR__.'/../../../autoload.php';
+
+// require "vendor/autoload.php";
+// require __DIR__."/../../autoload.php";
+// require_once __DIR__.'/../vendor/fiesta/kernel/src/Testing/TestCase.php';
+die(var_dump(glob(__DIR__."/*")));
 
 
 // Fiesta\Kernel\Foundation\Application::run("../",true,true,"Connector.php");
