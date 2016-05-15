e9d9fafcc21577654c8a9c5c0b868d16dd86e8c6
Youssef <youssefhad2@gmail.com>
2016-05-16 00:56:54 +0100

    test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,7 +7,7 @@
 // require "vendor/autoload.php";
 // require __DIR__."/../../autoload.php";
 // require_once __DIR__.'/../vendor/fiesta/kernel/src/Testing/TestCase.php';
-die(var_dump(glob(__DIR__."/../../*")));
+die(var_dump(glob(__DIR__."/../../../*")));
 
 
 // Fiesta\Kernel\Foundation\Application::run("../",true,true,"Connector.php");
