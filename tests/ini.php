7284723ec2b6b1442ef9cf649206d865b06d7966
Youssef <youssefhad2@gmail.com>
2016-05-15 14:34:29 +0100

    load framework for test

--- tests/ini.php
+++ tests/ini.php
@@ -1,9 +1,9 @@
 <?php
 
 
-require "vendor/autoload.php";
-// require __DIR__."/../../autoload.php";
+// die(var_dump(glob(__DIR__.'/../../../*')));
+require __DIR__.'/../../../autoload.php';
 
 
 // Fiesta\Kernel\Foundation\Application::run("../",true,true,"Connector.php");
-Fiesta\Kernel\Foundation\Application::test();
+Pikia\Kernel\Foundation\Application::test();
