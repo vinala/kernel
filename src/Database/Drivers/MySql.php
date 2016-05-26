f4dd4cd51b6d993ab5b35adfcb74b5f87151c61a
Youssef <youssefhad2@gmail.com>
2016-05-26 19:25:59 +0100

    put the export feature in driver class

--- src/Database/Drivers/MySql.php
+++ src/Database/Drivers/MySql.php
@@ -6,6 +6,9 @@ use Lighty\Kernel\Config\Config;
 use Lighty\Kernel\Database\Database;
 use Lighty\Kernel\Database\Exception\DatabaseArgumentsException;
 use Lighty\Kernel\Database\Exception\DatabaseConnectionException;
+use mysqli as Sql;
+use Lighty\Kernel\Objects\DateTime as Time;
+use Lighty\Kernel\Filesystem\Filesystem;
 
 
 /**
@@ -150,5 +153,165 @@ class MysqlDatabase
 	public static function res($sql)
 	{
 		return @mysqli_query(Database::$server,$sql);
+	}
+
+	public static function export()
+	{
+		$tables=false;
+		$backup_name=false;
+		//
+		$now = self::exportTimeU();
+	}
+
+	/**
+	 * get Data Table
+	 */
+	public static function databaseTables(Sql $sql)
+	{
+		$tables=false;
+		//
+		$Tables = $sql->query('SHOW TABLES'); 
+		//
+		while($row = $Tables->fetch_row()) 
+            $target_tables[] = $row[0];  
+        //
+        if($tables !== false) 
+            $target_tables = array_intersect( $target_tables, $tables); 
+        //
+        return $target_tables;
+	}
+
+	/**
+	 * Get the time of generation of the database
+	 */
+	protected static function exportTimeU()
+	{
+		return time();
+	}
+
+	/**
+	 * Get the time of generation of the database
+	 */
+	protected static function exportTime($time)
+	{
+		$current = Time::datetime($time);
+		//
+		return "-- Generated in $current ($time)";
+	}
+
+	/**
+	 * Get the info of the database
+	 */
+	protected static function exportInfo()
+	{
+		$database 	= Config::get("database.database");
+		$host 		= Config::get("database.host");
+		$username 	= Config::get("database.username");
+		//
+		return "\n\n-- Database : $database\n-- Host : $host\n-- User : $username\n";
+	}
+
+	/**
+	 * Get the Mysqli Class
+	 */
+	protected static function exportDatabase()
+	{
+		$database 	= Config::get("database.database");
+		$host 		= Config::get("database.host");
+		$username 	= Config::get("database.username");
+		$password 	= Config::get("database.password");
+		//
+		$mysqli = new \mysqli($host,$username,$password,$database); 
+        $mysqli->select_db($database); 
+        $mysqli->query("SET NAMES 'utf8'");
+        //
+        return $mysqli;
+	}
+
+	/**
+	 * Get the Mysqli Class
+	 */
+	protected static function saveExport($time, $content)
+	{
+		$database 	= Config::get("database.database");
+		//
+		$name = $database."_".$time.".sql";
+		$path = "app/database/$name";
+		//
+		return (new Filesystem)->put($path,$content); 
+	}
+
+	
+
+	/**
+	 * Export the Database
+	 */
+	public static function export()
+	{
+		$backup_name=false;
+		//
+		$now = self::exportTimeU();
+		//
+		$content 	= self::exportTime($now).self::exportInfo();
+		$mysqli 	= self::exportDatabase();
+		$tables    	= self::databaseTables($mysqli); 
+		//
+        foreach($target_tables as $table)
+        {
+            $result         =   $mysqli->query('SELECT * FROM '.$table);  
+            $fields_amount  =   $result->field_count;  
+            $rows_num=$mysqli->affected_rows;     
+            $res            =   $mysqli->query('SHOW CREATE TABLE '.$table); 
+            $TableMLine     =   $res->fetch_row();
+            $content        = (!isset($content) ?  '' : $content) . "\n\n".$TableMLine[1].";\n\n";
+
+            for ($i = 0, $st_counter = 0; $i < $fields_amount;   $i++, $st_counter=0) 
+            {
+                while($row = $result->fetch_row())  
+                { //when started (and every after 100 command cycle):
+                    if ($st_counter%100 == 0 || $st_counter == 0 )  
+                    {
+                            $content .= "\nINSERT INTO ".$table." VALUES";
+                    }
+                    $content .= "\n(";
+                    for($j=0; $j<$fields_amount; $j++)  
+                    { 
+                        $row[$j] = str_replace("\n","\\n", addslashes($row[$j]) ); 
+                        if (isset($row[$j]))
+                        {
+                            $content .= '"'.$row[$j].'"' ; 
+                        }
+                        else 
+                        {   
+                            $content .= '""';
+                        }     
+                        if ($j<($fields_amount-1))
+                        {
+                                $content.= ',';
+                        }      
+                    }
+                    $content .=")";
+                    //every after 100 command cycle [or at last line] ....p.s. but should be inserted 1 cycle eariler
+                    if ( (($st_counter+1)%100==0 && $st_counter!=0) || $st_counter+1==$rows_num) 
+                    {   
+                        $content .= ";";
+                    } 
+                    else 
+                    {
+                        $content .= ",";
+                    } 
+                    $st_counter=$st_counter+1;
+                }
+            } $content .="\n\n\n";
+        }
+        //$backup_name = $backup_name ? $backup_name : $name."___(".date('H-i-s')."_".date('d-m-Y').")__rand".rand(1,11111111).".sql";
+        $backup_name = $backup_name ? $backup_name : $now.".sql";
+        header('Content-Type: application/octet-stream');   
+        header("Content-Transfer-Encoding: Binary"); 
+        header("Content-disposition: attachment; filename=\"".$backup_name."\"");
+        //
+        self::saveExport($now, $content);
+        //
+        return true;
 	}
 }
