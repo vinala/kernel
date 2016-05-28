03ab6aaf29e9d72df2982c05e96f688098b38e6b
Youssef <youssefhad2@gmail.com>
2016-05-28 22:28:26 +0100

    make new seeders with columns name

--- src/Processes/Seeds.php
+++ src/Processes/Seeds.php
@@ -3,6 +3,7 @@
 namespace Lighty\Kernel\Process;
 
 use Lighty\Kernel\Database\Seeder;
+use Lighty\Kernel\Database\Database;
 use Lighty\Kernel\Foundation\Application;
 
 /**
@@ -36,6 +37,8 @@ class Seeds
 
 	public static function set($nom,$table)
 	{
+		$colmuns = self::colmuns($table);
+		//
 		$txt = "<?php\n\nuse Lighty\Kernel\Database\Seeder;\n\n";
 		$txt.="/**\n* class de seeder $nom\n*/\nclass $nom extends Seeder\n{\n";
 
@@ -43,9 +46,20 @@ class Seeds
 		$txt.="\t/*\n\t* Name of DataTable\n\t*/\n\tpublic ".'$table="'.$table.'";'."\n\n";
 
 			//run
-		$txt.="\t/*\n\t* Run the Database Seeder\n\t*/\n\tpublic function run()\n\t{\n\t\t".'$data = array();'."\n\t\t//"."\n\t\t".'for ($i=0; $i < 50; $i++)'."\n\t\t\t".'Table::push($data , array(/* Data Fields */));'."\n\t\t//\n\t\t".'return Schema::table($this->table)->insert($data);'."\n\t}\n}";
+		$txt.="\t/*\n\t* Run the Database Seeder\n\t*/\n\tpublic function run()\n\t{\n\t\t".'$data = array();'."\n\t\t//"."\n\t\t".'for ($i=0; $i < 50; $i++)'."\n\t\t\t".'Table::push($data , array('.$colmuns."\n\t\t\t".'));'."\n\t\t//\n\t\t".'return Schema::table($this->table)->insert($data);'."\n\t}\n}";
 
 		return $txt;
+	}
+
+	public static function colmuns($table)
+	{
+		$colmuns = Database::normalColumns($table);
+		$str = "";
+		//
+		foreach ($colmuns as $value) 
+			$str .= "\n\t\t\t\t".'"'.$value.'" => ,';
+		//
+		return $str;
 	}
 
 	/** 
