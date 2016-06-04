e6df8032330aa76b809f235fe7297fc1a7009b7b
Youssef <youssefhad2@gmail.com>
2016-06-04 12:30:54 +0100

    make response for panel and secure setup and change configured parameter to setup

--- src/Setup/Response.php
+++ src/Setup/Response.php
@@ -102,6 +102,46 @@ class Response
 		}
 	}
 
+	/**
+	 * set security setup
+	 */
+	public static function setSecur_step()
+	{
+		$sec_1=$_POST['sec_1'];
+		$sec_2=$_POST['sec_2'];
+		//
+		if( ! Application::$isTest) 
+		{
+			file_put_contents(Application::$root."config/security.php", self::securityCont($sec_1,$sec_2), 0);
+			//
+			echo "true";
+		}
+	}
+
+	/**
+	 * set panel setup
+	 */
+	public static function setPanel_step()
+	{
+		if(isset($_POST['stat'])) $state="true";
+		else $state="false";
+		//
+		$route=empty($_POST['route']) ? "lighty" : $_POST['route'];
+		$pass_1=empty($_POST['pass_1']) ? "1234" : $_POST['pass_1'];
+		$pass_2=empty($_POST['pass_2']) ? "5678" : $_POST['pass_2'];
+		//
+		if( ! Application::$isTest) 
+		{
+			file_put_contents(Application::$root."config/panel.php", self::panelCont($state,$route,$pass_1,$pass_2), 0);
+			//
+			echo "true";
+		}
+	}
+
+
+
+
+
 
 //******************************
 
@@ -340,7 +380,7 @@ class Response
 			'route' => "\n\t|  Route for panel, for your security please change it",
 			'path' => "\n\t|  Here the path of the panel index, you can \n\t|  search in the internet to change the panel, \n\t|  for your security you should change the panel\n\t|  folder name",
 			'passwords' => "\n\t|  Here are the passwords to access to the panel",
-			'configuration' => "\n\t|  The framework will set true if you passed \n\t|  the first configuration",
+			'configuration' => "\n\t|  The framework will set true if you passed \n\t|  the setup",
 			'ajax' => "\n\t|  This is links of ajax functions",
 			);
 		//
@@ -354,7 +394,7 @@ class Response
 			'route' => "Panel Route",
 			'path' => "Panel Path",
 			'passwords' => "Panel Passwords",
-			'configuration' => "First Configuration",
+			'configuration' => "Setup",
 			'ajax' => "Ajax Routes",
 			);
 		//
@@ -376,7 +416,7 @@ class Response
 		$route = self::panelRow("route","'route'=>'$route',");
 		$path = self::panelRow("path","'path'=>'vendor/lighty/panel/index.php',");
 		$passwords = self::panelRow("passwords","'password1'=>'$pass_1',\n\t'password2'=>'$pass_2',");
-		$configuration = self::panelRow("configuration","'configured' => true,");
+		$configuration = self::panelRow("configuration","'setup' => true,");
 		$ajax = self::panelRow("ajax","'ajax' => array(\n\n\t\t// for new seeds\n\t\t\t'new_seed' => 'new_seed',\n\n\t\t// to exec migrations\n\t\t\t'exec_migration' => 'exec_migration',\n\n\t\t// to rollback migrations\n\t\t\t'rollback_migration' => 'rollback_migration', \n\n\t\t// for new migrations\n\t\t\t'new_migration' => 'new_migration',\n\n\t\t// for new controllers\n\t\t\t'new_controller' => 'new_controller',\n\n\t\t// for new language folder\n\t\t\t'new_dir_lang' => 'new_dir_lang',\n\n\t\t// for new language file\n\t\t\t'new_file_lang' => 'new_file_lang',\n\n\t\t// for new links file\n\t\t\t'new_link' => 'new_link',\n\n\t\t// for new models\n\t\t\t'new_model' => 'new_model',\n\n\t\t// for new views\n\t\t\t'new_view' => 'new_view',\n\n\t\t// to exec costume migrations\n\t\t\t'exec_cos_migration' => 'exec_cos_migration',\n\n\t\t// to rollback costume migrations\n\t\t\t'rollback_cos_migration' => 'rollback_cos_migration',\n\t),");
 		//
 		return "<?php \n\nreturn array(\n\t".$activation.$route.$path.$passwords.$configuration.$ajax."\n);";
