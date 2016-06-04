1b51cbc676a8725a41bf5d2cd5a9733083287908
Youssef <youssefhad2@gmail.com>
2016-06-04 12:27:28 +0100

    add route for panel and secure setup

--- src/Setup/Routes.php
+++ src/Setup/Routes.php
@@ -4,3 +4,5 @@ use Lighty\Kernel\Setup\Response;
 
 get("hello/db_check",function() { return Response::setDb_step(); });
 get("hello/set_glob",function() { return Response::setGlob_step(); });
+get("hello/set_secur",function() { return Response::setSecur_step(); });
+get("hello/set_panel",function() { return Response::setPanel_step(); });
