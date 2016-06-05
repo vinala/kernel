b87d650496feb645a59cccdc64a6a6c00fc297c5
Youssef <youssefhad2@gmail.com>
2016-06-05 15:09:40 +0000

    new Documentations setup for robots.txt

---
+++ src/Setup/Documentations/Robots.php
@@ -0,0 +1,22 @@
+<?php 
+namespace Lighty\Kernel\Setup\Documentations;
+
+use Lighty\kernel\Foundation\Application;
+
+class Robots
+{
+	public static function set($hide = true)
+	{
+		return self::write(self::count($hide));
+	}
+
+	public static function count($hide)
+	{
+		return "User-agent: *\nDisallow:". ( $hide ? " / " : "");
+	}
+
+	public static function write($count)
+	{
+		return file_put_contents(Application::$root."robots.txt", $count , 0);
+	}
+}
