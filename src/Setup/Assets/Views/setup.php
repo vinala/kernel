96ec2bdea77d4247b9ea413584a2c4e63c12e209
Youssef <youssefhad2@gmail.com>
2016-06-05 14:35:21 +0000

    add switch to ask search engines not to index the project

--- src/Setup/Assets/Views/setup.php
+++ src/Setup/Assets/Views/setup.php
@@ -143,6 +143,17 @@
 			</div>
 
 			<div class="control_c_row">
+				<div class="fst-config-label db-input-label">Hide from search engines</div>
+				<div class="fst-config-input db-input-input">
+					<div class="switch">
+					    <input type="checkbox" name="ckeck_search" class="switch-checkbox" id="chechBox6" checked>
+					    <label class="switch-label switch-label-violet" for="chechBox6"></label>
+					</div>
+				</div>
+				<div class="fst-config-input-desc db-input-input-desc label-text-switch">Ask search engines not to index this site</div>
+			</div>
+
+			<div class="control_c_row">
 				<div class="fst-config-label db-input-label">Debugging</div>
 				<div class="fst-config-input db-input-input">
 					<div class="switch">
@@ -150,18 +161,18 @@
 					    <label class="switch-label switch-label-violet" for="chechBox1"></label>
 					</div>
 				</div>
-				<div class="fst-config-input-desc db-input-input-desc">Do you want to enable debugging</div>
+				<div class="fst-config-input-desc db-input-input-desc label-text-switch">Do you want to enable debugging</div>
 			</div>
 
 			<div class="control_c_row">
 				<div class="fst-config-label db-input-label">Maintenance</div>
 				<div class="fst-config-input db-input-input">
 					<div class="switch">
-					    <input type="checkbox" name="ckeck_maintenance" class="switch-checkbox" id="chechBox2" >
-					    <label class="switch-label switch-label-violet" for="chechBox2"></label>
+					    <input type="checkbox" name="ckeck_maintenance" class="switch-checkbox" id="chechBox5">
+					    <label class="switch-label switch-label-violet" for="chechBox5"></label>
 					</div>
 				</div>
-				<div class="fst-config-input-desc db-input-input-desc">You can change the error message and maintenance later in the configuration files</div>
+				<div class="fst-config-input-desc db-input-input-desc label-text-switch">You can change the error message and maintenance later in the configuration files</div>
 			</div>
 
 			<div style="margin-top:20px">
@@ -262,150 +273,6 @@
 		</div>
 	</div>
 
-
-
-
-	<div id="etap_1"  style="display:none">
-		<h1 class="conf_title">General</h1>
-		<div class="progres"><div class="progres_1"></div></div>
-		<form class="config_form" id="form_1">
-			<div class="control_c_row">
-				<div class="conf_lab">
-					<label for="">Developer Name</label>
-				</div>
-				<div class="conf_input">
-					<input type="text" class="form-control" id="dev_name" name="dev_name" placeholder="Your name">
-				</div>
-			</div>
-			<div class="control_c_row">
-				<div class="conf_lab">
-					<label for="sel1">Language</label>
-				</div>
-				<div class="conf_input">
-					<select class="form-control" id="sel1" name="langue">
-					    <option value="fr">Français</option>
-						<option value="ar">العربية</option>
-						<option value="en" selected>English</option>
-			        </select>
-				</div>
-			</div>
-
-			<div class="control_c_row">
-				<div class="conf_lab">
-					<label for="sel1">Debugging</label>
-				</div>
-				<div class="conf_input">
-			        <div class="switch">
-					    <input type="checkbox" name="ckeck_loggin" class="switch-checkbox" id="chechBox1" >
-					    <label class="switch-label switch-label-violet" for="chechBox1"></label>
-					</div>
-				</div>
-			</div>
-
-			<div class="control_c_row">
-				<div class="conf_lab">
-					<label for="sel1">Maintenance</label>
-				</div>
-				<div class="conf_input">
-			        <div class="switch">
-					    <input type="checkbox" name="ckeck_maintenance" class="switch-checkbox" id="chechBox2" >
-					    <label class="switch-label switch-label-violet" for="chechBox2"></label>
-					</div>
-					<p class="conf_input_note">
-					You can change the error message and maintenance later in the configuration files</p>
-				</div>
-			</div>
-
-			<!-- <div class="bottom" id="bottom_panel">
-				<a href="<?php echo Config::get("panel.route") ?>"><div class="btn hello_button" id="login">Lighty Panel</div></a>
-			</div> -->
-			<div style="margin-top:20px">
-				<input type="submit" class="btn hello_button" value="Next" name="nxt" id="nxt"   />
-			</div>
-		</form>
-	</div>
-
-	<div id="etap_3" style="display:none">
-		<h1 class="conf_title">Security</h1>
-		<div class="progres"><div class="progres_3"></div></div>
-		<form class="config_form" id="form_3">
-			<div class="control_c_row">
-				<div class="conf_lab">
-					<label for="">Security key 1</label>
-				</div>
-				<div class="conf_input">
-					<input type="text" class="form-control" id="migname" name="sec_1" placeholder="Server" value="<?php echo md5(uniqid(rand(), TRUE)) ?>" readonly>
-				</div>
-			</div>
-			<div class="control_c_row">
-				<div class="conf_lab">
-					<label for="">Security key 2</label>
-				</div>
-				<div class="conf_input">
-					<input type="text" class="form-control" id="migname" name="sec_2" placeholder="Database" value="<?php echo md5(uniqid(rand(), TRUE)) ?>" readonly>
-				</div>
-			</div>
-			<div style="margin-top:20px">
-				<input type="submit" class="btn hello_button" value="Next" name="nxt" id="nxt"   />
-			</div>
-		
-			
-		</form>
-	</div>
-
-	<div id="etap_4" style="display:none">
-		<h1 class="conf_title">Panel</h1>
-		<div class="progres"><div class="progres_4"></div></div>
-		<form class="config_form" id="form_4">
-			<div class="control_c_row">
-				<div class="conf_lab">
-					<label for="sel1">Activation</label>
-				</div>
-				<div class="conf_input">
-					<!-- <select class="form-control" id="sel1" name="state">
-					    <option value="true" selected>Activé</option>
-						<option value="false">Désactivé</option>
-			        </select> -->
-			        <div class="switch">
-					    <input type="checkbox" name="stat" class="switch-checkbox" id="myswitch-violet" checked>
-					    <label class="switch-label switch-label-violet" for="myswitch-violet"></label>
-					</div>
-				</div>
-			</div>
-			<div class="control_c_row">
-				<div class="conf_lab">
-					<label for="">Route</label>
-				</div>
-				<div class="conf_input">
-					<input type="text" class="form-control" id="pnl_route" name="route" placeholder="Route" value="" >
-					<p class="conf_input_note">By default : Lighty</p>
-				</div>
-			</div>
-			<div class="control_c_row">
-				<div class="conf_lab">
-					<label for="">Password 1</label>
-				</div>
-				<div class="conf_input">
-					<input type="text" class="form-control" id="migname" name="pass_1" placeholder="Password 1" value="">
-					<p class="conf_input_note">By default : 1234</p>
-				</div>
-			</div>
-			<div class="control_c_row">
-				<div class="conf_lab">
-					<label for="">Password 2</label>
-				</div>
-				<div class="conf_input">
-					<input type="text" class="form-control" id="migname" name="pass_2" placeholder="Password 2" value="">
-					<p class="conf_input_note">By default : 5678</p>
-				</div>
-			</div>
-			<div style="margin-top:20px">
-				<input type="submit" class="btn hello_button" value="Finish"   />
-			</div>
-		
-			
-		</form>
-	</div>
 
 	<img src="<?php echo "app/resources/images/lighty_logo.png" ?>" class="img" id="hello_logo" style="display:none">
 
