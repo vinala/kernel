2f39402d033630faf6bdfc48d08888c8106fd6a7
Youssef <youssefhad2@gmail.com>
2016-06-16 00:59:23 +0000

    update sub atomium compiler

--- src/Atomium/Compiler/AtomiumCompileSub.php
+++ src/Atomium/Compiler/AtomiumCompileSub.php
@@ -2,58 +2,11 @@
 
 namespace Lighty\Kernel\Atomium\Compiler;
 
-use Lighty\Kernel\Objects\Strings;
-use Lighty\Kernel\Objects\Table;
-
 /**
 * 
 */
 class AtomiumCompileSub
 {
-
-	/**
-	 * The tag that open the instruction 
-	 *
-	 * @var string
-	 */
-	protected static $openTag = "@sub";
-
-
-	/**
-	 * The key where open tag end's
-	 *
-	 * @var string
-	 */
-	protected static $endOpenTag = ")";
-
-
-	/**
-	 * The PHP Open tag that should Lighty replace with
-	 *
-	 * @var string
-	 */
-	protected static $phpOpenTag = "<?php View::make" ;
-
-
-	/**
-	 * The key where PHP Open tag end's
-	 *
-	 * @var string
-	 */
-	protected static $phpEndOpenTag = "); ?> " ;
-
-
-	/**
-	 * Complie the opening tag
-	 *
-	 * @var string
-	 */
-	protected static function openTag($script)
-	{
-		return AtomiumCompileInstruction::openTag($script, self::$openTag, self::$phpOpenTag, self::$endOpenTag, self::$phpEndOpenTag);
-	}
-
-
 	/**
 	 * run the compiler
 	 *
@@ -61,7 +14,6 @@ class AtomiumCompileSub
 	 */
 	public static function run($script)
 	{
-		$script = self::openTag($script);
-		return $script;
+		return AtomiumCompileOneLineInstruction::run($script, "@sub", ";", "<?php View::make(" ,"); ?>");
 	}
 }
