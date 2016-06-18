a6f64d5659c1ca29fefb12a0eff20f9c6dbaa238
Youssef <youssefhad2@gmail.com>
2016-06-18 22:49:57 +0000

    add routes for ajax request

--- src/Dashboard/Dashboard.php
+++ src/Dashboard/Dashboard.php
@@ -5,6 +5,8 @@ namespace Lighty\Kernel\Dashboard;
 use Lighty\Kernel\Foundation\Application;
 use Lighty\Kernel\Config\Alias;
 use Lighty\Kernel\Config\Config;
+use Lighty\Kernel\Router\Route;
+use Lighty\Kernel\Dashboard\Response;
 
 /**
 * Controller class
@@ -20,11 +22,32 @@ class Dashboard
 		self::$assets = "vendor/lighty/kernel/src/Dashboard/";
 		//
 		Alias::set(\Lighty\Kernel\Dashboard\Dashboard::class, 'Dashboard');
+		//
+		self::route();
 	}
 
 	public static function getOwner()
 	{
 		return Config::get("app.owner");
+	}
+
+	public static function route()
+	{
+		Route::get(Config::get('dashboard.token')."_/{op}",function($op){
+			switch ($op) {
+				case 'new_model': Response::createModel();  break;
+			}
+		});
+
+	}
+
+	public static function view($path, $data = null)
+	{
+		if($data)
+			foreach ($data as $key => $value)
+				$$key = $value;
+		//
+		include(Dashboard::$root.'views/'.$path.'.php');
 	}
 
 
