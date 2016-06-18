eae2c30777a8233cf0b204fac42bd5a4aa3e3ed9
Youssef <youssefhad2@gmail.com>
2016-06-18 20:29:10 +0000

    add view forpagebar

---
+++ src/Dashboard/views/parts/pageBar.php
@@ -0,0 +1,37 @@
+<div class="page-bar">
+        <ul class="page-breadcrumb">
+            <li>
+                <a href="index-2.html"><?php echo $main ?></a>
+                <i class="fa fa-circle"></i>
+            </li>
+            <li>
+                <span><?php echo $title ?></span>
+            </li>
+        </ul>
+        <div class="page-toolbar">
+            <div class="btn-group pull-right">
+                <button type="button" class="btn green btn-sm btn-outline dropdown-toggle" data-toggle="dropdown"> Actions
+                    <i class="fa fa-angle-down"></i>
+                </button>
+                <ul class="dropdown-menu pull-right" role="menu">
+                    <li>
+                        <a href="#">
+                            <i class="icon-bell"></i> Action</a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="icon-shield"></i> Another action</a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="icon-user"></i> Something else here</a>
+                    </li>
+                    <li class="divider"> </li>
+                    <li>
+                        <a href="#">
+                            <i class="icon-bag"></i> Separated link</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
