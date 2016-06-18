653c18d3f44be62266eae23fe37d0c815c5a4341
Youssef <youssefhad2@gmail.com>
2016-06-18 17:36:43 +0000

    add propriety root for assets

--- src/Dashboard/Dashboard.php
+++ src/Dashboard/Dashboard.php
@@ -11,11 +11,12 @@ use Lighty\Kernel\Config\Alias;
 class Dashboard
 {
 	public static $root;
+	public static $assets;
 
 	public static function ini()
 	{
 		self::$root = Application::$root."vendor/lighty/kernel/src/Dashboard/";
-		self::$root = "vendor/lighty/kernel/src/Dashboard/";
+		self::$assets = "vendor/lighty/kernel/src/Dashboard/";
 		//
 		Alias::set(\Lighty\Kernel\Dashboard\Dashboard::class, 'Dashboard');
 	}
