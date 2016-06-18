d979477546bbd868da9ddce8ef129b4850d0d102
Youssef <youssefhad2@gmail.com>
2016-06-18 22:54:37 +0000

    add Panel vars and use request js file

--- src/Dashboard/views/bars/plugins.php
+++ src/Dashboard/views/bars/plugins.php
@@ -3,6 +3,9 @@
 <script src="../assets/global/plugins/excanvas.min.js"></script> 
 <![endif]-->
 
+
+<script src="<?php echo Dashboard::$assets ?>assets/global/plugins/jquery.min.js" type="text/javascript"></script>
+
 <!-- BEGIN CORE PLUGINS -->
 <script src="<?php echo Dashboard::$assets ?>assets/global/plugins/jquery.min.js" type="text/javascript"></script>
 <script src="<?php echo Dashboard::$assets ?>assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
@@ -56,10 +59,7 @@
 <script src="<?php echo Dashboard::$assets ?>assets/layouts/global/scripts/quick-sidebar.min.js" type="text/javascript"></script>
 <!-- END THEME LAYOUT SCRIPTS -->
 <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','<?php echo Dashboard::$assets ?><?php echo Dashboard::$assets ?><?php echo Dashboard::$assets ?><?php echo Dashboard::$assets ?><?php echo Dashboard::$assets ?>www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-37564768-1', 'keenthemes.com');
-    ga('send', 'pageview');
+    var route = "<?php echo Config::get('dashboard.route') ?>";
+    var token = "<?php echo Config::get('dashboard.token') ?>";
 </script>
+<script src="<?php echo Dashboard::$assets ?>assets/js/request.js" type="text/javascript"></script>
