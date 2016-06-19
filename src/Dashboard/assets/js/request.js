1ac85ea3828ac0910b4eb0fe65cf2874250f959b
Youssef <youssefhad2@gmail.com>
2016-06-19 00:57:02 +0000

    update url to post to

--- src/Dashboard/assets/js/request.js
+++ src/Dashboard/assets/js/request.js
@@ -1,13 +1,15 @@
 $(document).ready(function (){
     $('#new_model_form').submit(function () {
-        $.post(token+"_/new_model",$('#new_model_form').serialize(),function(data)
+        $.post("../"+token+"_/new_model",$('#new_model_form').serialize(),function(data)
             {
                 if(data == "true")
                 {
                     $( "#new_models_alert_succes" ).slideToggle( "slow" );
                     setTimeout(function(){ $( "#new_models_alert_succes" ).slideToggle( "slow" ); }, 3000);
                 }
-                else $( "#new_models_alert_failed" ).slideToggle( "slow" );}
-        }); return false;
+                else $( "#new_models_alert_failed" ).slideToggle( "slow" );
+            });
+        //
+        return false;
     });
 });
