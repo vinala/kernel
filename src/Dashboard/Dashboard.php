68fbe52868fcd56cb8f13500022c55311b8232da
Youssef <youssefhad2@gmail.com>
2016-06-19 00:59:06 +0000

    update roots

--- src/Dashboard/Dashboard.php
+++ src/Dashboard/Dashboard.php
@@ -19,7 +19,7 @@ class Dashboard
 	public static function ini()
 	{
 		self::$root = Application::$root."vendor/lighty/kernel/src/Dashboard/";
-		self::$assets = "vendor/lighty/kernel/src/Dashboard/";
+		self::$assets = Application::$root."vendor/lighty/kernel/src/Dashboard/";
 		//
 		Alias::set(\Lighty\Kernel\Dashboard\Dashboard::class, 'Dashboard');
 		//
@@ -31,8 +31,15 @@ class Dashboard
 		return Config::get("app.owner");
 	}
 
+	protected static function callHome($page)
+	{
+		include_once '../vendor/lighty/kernel/src/Dashboard/views/home.php';  
+	}
+
 	public static function route()
 	{
+		Route::get(Config::get('dashboard.route')."/model",function(){ self::callHome("model"); });
+		//
 		Route::get(Config::get('dashboard.token')."_/{op}",function($op){
 			switch ($op) {
 				case 'new_model': Response::createModel();  break;
