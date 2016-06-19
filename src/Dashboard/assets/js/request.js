287ba4b494c3b31b2ea4355fe2f13e31da2dce59
Youssef <youssefhad2@gmail.com>
2016-06-19 22:11:08 +0000

    add new javascript request for new view

--- src/Dashboard/assets/js/request.js
+++ src/Dashboard/assets/js/request.js
@@ -1,4 +1,9 @@
 $(document).ready(function (){
+
+    
+    /*
+     * NEW MODEL
+     */
     $('#new_model_form').submit(function () {
         $.post("../"+token+"_/new_model",$('#new_model_form').serialize(),function(data)
             {
@@ -8,6 +13,25 @@ $(document).ready(function (){
                     setTimeout(function(){ $( "#new_models_alert_succes" ).slideToggle( "slow" ); }, 3000);
                 }
                 else $( "#new_models_alert_failed" ).slideToggle( "slow" );
+            });
+        //
+        return false;
+    });
+
+
+
+    /*
+     * NEW VIEW
+     */
+    $('#new_view_form').submit(function () {
+        $.post("../"+token+"_/new_view",$('#new_view_form').serialize(),function(data)
+            {
+                if(data == "true")
+                {
+                    $( "#new_view_alert_succes" ).slideToggle( "slow" );
+                    setTimeout(function(){ $( "#new_view_alert_succes" ).slideToggle( "slow" ); }, 3000);
+                }
+                else $( "#new_view_alert_failed" ).slideToggle( "slow" );
             });
         //
         return false;
