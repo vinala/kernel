0cc14358cb5a154ff1f8db7f7202f1dd5e66cb2e
Youssef <youssefhad2@gmail.com>
2016-09-25 16:23:28 +0100

    add new schema exception for table if existe and not existe

---
+++ src/Database/Exceptions/SchemaTableExistsException.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace Lighty\Kernel\Database\Schema\Exception;
+
+/**
+* Directory not fount exception
+*/
+class SchemaTableExistsException extends \Exception
+{
+	function __construct($table) 
+	{
+		$this->message = "The table '$table' already exists";
+	}
+}
---
+++ src/Database/Exceptions/SchemaTableNotExistException.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace Lighty\Kernel\Database\Schema\Exception;
+
+/**
+* Directory not fount exception
+*/
+class SchemaTableNotExistException extends \Exception
+{
+	function __construct($table) 
+	{
+		$this->message = "The table '$table' doesn't exist";
+	}
+}
