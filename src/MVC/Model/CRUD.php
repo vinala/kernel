9e938c9e0fec98eb3bd3218ba637889fbb0e384c
Youssef <youssefhad2@gmail.com>
2016-10-02 00:37:53 +0100

    add class for CRUD state

---
+++ src/MVC/Model/CRUD.php
@@ -0,0 +1,36 @@
+<?php 
+
+namespace Lighty\Kernel\MVC\ORM ;
+
+
+/**
+* The Create - Read - Update - Delete (CRUD) Class of ORM Structure
+*/
+class CRUD
+{
+
+	//--------------------------------------------------------
+	// Constants
+	//--------------------------------------------------------
+
+	/**
+	* constant for new data creating
+	*
+	* @var string
+	*/
+	const CREATE_STAT = "create" ;
+
+	/**
+	* constant for edit data 
+	*
+	* @var string
+	*/
+	const UPDATE_STAT = "update" ;
+
+
+	//--------------------------------------------------------
+	// Functions
+	//--------------------------------------------------------
+
+	
+}
