24521357aa527b9668e315330082de30daa2d1f7
Youssef <youssefhad2@gmail.com>
2016-10-25 22:45:28 +0100

    update setup command to just generate security keys while composer make create project more interactive

--- src/Lumos/Commands/SetupCommand.php
+++ src/Lumos/Commands/SetupCommand.php
@@ -34,30 +34,6 @@ class SetupCommand extends Commands
     {
         $this->line("\nWelcome to Vinala Framework");
         $this->line("by Youssef Had (www.facebook.com/yussef.had)");
-        $this->line("");
-        $name = $this->ask("What's your name ?");
-        $this->line("Welcome ".$name);
-        
-        //
-        if( $this->confirm("\nBefore we launch $name, would you like to fill some information about your project ? [y/n] " , true) )
-        {
-           
-            $this->line("");
-            $project = $this->ask("What's your project name ?");
-            $lang = $this->choice("What's your app language ?" , ['english' , 'french' , 'arab']);
-            $hide = $this->confirm("Wanna hide your app from search engines ? [y/n]" , false);
-            $debugging = $this->confirm("Activate debugging mode ? [y/n]" , true);
-            //
-            $panel = $this->confirm("\nThe framework also use a panel controle , would you like to activate it ? [y/n]" , true);
-            if($panel)
-            {
-                $route = $this->ask("What's the panel route?");
-                $password1 = $this->ask("What's the first password ?");
-                $password2 = $this->ask("What's the second password ?");
-                //
-                Response::setPanel_step($panel, $route , $password1 , $password2);
-            }
-        }
         //
         Response::setGlob_step($project , $name , $lang , $debugging , $hide);
         $key1 = md5(uniqid(rand(), TRUE));
@@ -69,18 +45,6 @@ class SetupCommand extends Commands
         $this->info("$key1");
         $this->write("second key : ");
         $this->info("$key2");
-        $this->line("\nThe framework is ready\n");
-    }
-
-    /**
-     * Format the message to show
-    */
-    public function show()
-    {
-        
-        
-        
-
     }
 }
 
