424c41f741b46aa9dd93079b767048b78e6f7cd2
Youssef <youssefhad2@gmail.com>
2016-11-08 21:53:01 +0000

    add open function and get method function

--- src/Html/Form.php
+++ src/Html/Form.php
@@ -8,4 +8,32 @@ namespace Vinala\Kernel\Html\Form ;
 class Form
 {
 
+	/**
+	* function to open the form
+	*
+	* @param array $options
+	* @return string
+	*/
+	public static function open( array $options = array() )
+	{
+		// get the method passed in $options else use port
+		$method = array_get($options, 'method', 'post');
+		return ;
+	}
+
+	/**
+	* function to set form method to upper 
+	* and eccept ONLY get and post
+	*
+	* @param string $method
+	* @return string
+	*/
+	protected static function getMethod($method)
+	{
+		$method = strtoupper($method);
+		//
+		return $method != "GET" ? "POST" : $method;
+	}
+	
+	
 }
