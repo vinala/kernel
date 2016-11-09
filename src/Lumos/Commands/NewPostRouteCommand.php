f46056d15c262517232f49aa263f6418c00fd744
Youssef <youssefhad2@gmail.com>
2016-11-09 14:50:17 +0000

    add lumos command for new post route

---
+++ src/Lumos/Commands/NewPostRouteCommand.php
@@ -0,0 +1,65 @@
+<?php 
+
+namespace Vinala\Kernel\Console\Commands;
+
+
+use Vinala\Kernel\Config\Config;
+use Vinala\Kernel\Console\Command\Commands;
+use Vinala\Kernel\Process\Router;
+
+
+class NewPostRouteCommand extends Commands
+{
+
+    /**
+     * The key of the console command.
+     *
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    public $description;
+
+    /**
+     * Configure the command
+     */ 
+    public function set()
+    {
+        $this->key = config('lumos.post_routes').' {http : what\'s the http of route?}';
+        $this->description = 'Add new post route to Routes file';
+    }
+
+    /**
+     * Handle the command
+     */
+    public function handle()
+    {
+        $this->exec();
+    }
+
+    /**
+     * Execute the command
+     */
+    public function exec()
+    {
+        $http = $this->argument('http');
+        //
+        $process = Router::post($http);
+        //
+        $this->show($process);
+    }
+
+    /**
+     * Format the message to show
+    */
+    public function show($process)
+    {
+        if($process) $this->info("The route was created");
+        else $this->error("The route doesn't created");
+    }
+}
