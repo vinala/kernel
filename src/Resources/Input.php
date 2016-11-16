8f38e1ebed2b94efca20ffae9445b8ac84bc1fa5
Youssef <youssefhad2@gmail.com>
2016-11-16 23:02:48 +0000

    add function to get  GET and POST vars

--- src/Resources/Input.php
+++ src/Resources/Input.php
@@ -5,5 +5,16 @@
 */
 class Input
 {
-	
+	/**
+		* Get the input POST or GET vars
+		*
+		* @param string $key
+		* @param string $method
+		* @return string
+		*/
+		public static function get($key , $method = null)
+		{
+			
+		}
+			
 }
