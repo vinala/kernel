54689e475b71ac6985503e52bda8dafa8bd2ed73
Youssef <youssefhad2@gmail.com>
2016-11-17 23:51:55 +0000

    small update

--- src/Resources/Input.php
+++ src/Resources/Input.php
@@ -5,16 +5,5 @@
 */
 class Input
 {
-	/**
-		* Get the input POST or GET vars
-		*
-		* @param string $key
-		* @param string $method
-		* @return string
-		*/
-		public static function get($key , $method = null)
-		{
-			
-		}
-			
+				
 }
