4817f4d307aba912da34d18e2f8e843db4919da8
Youssef <youssefhad2@gmail.com>
2016-11-18 16:17:34 +0000

    add reach and register function

--- src/Http/Input.php
+++ src/Http/Input.php
@@ -8,6 +8,48 @@ namespace Vinala\Kernel\Http ;
 class Input
 {
 	
+
+	/**
+	* list of alla input vars
+	*
+	* @var array 
+	*/
+	protected static $list ;
+
+
+	/**
+	* regsiter all http input vars
+	*
+	* @return array
+	*/
+	public static function regsiter()
+	{
+		self::$list = array();
+		//
+		self::$list['post'] = $_POST;
+		self::$list['get'] = $_GET;
+		self::$list['session'] = $_SESSION;
+		self::$list['cookie'] = $_COOKIE;
+		self::$list['files'] = $_FILES;
+		self::$list['server'] = $_SERVER;
+		self::$list['env'] = $_ENV;
+		self::$list['request'] = $_REQUEST;
+		return self::$list;
+	}
+	
+	/**
+	* get any http vars by 'dot' notation
+	*
+	* @param string $key
+	* @param mixed $default
+	* @return mixed
+	*/
+	public static function reach($key , $default = null)
+	{
+		return array_get(self::$list , $key , $default);
+	}
+	
+
 	/**
 	* get $_GET input vars
 	*
@@ -17,8 +59,10 @@ class Input
 	*/
 	public static function get($key , $default = null)
 	{
-		return array_get($_GET , $key , $default);
+		return array_get(self::$list , 'get.'.$key , $default);
 	}
+
+
 	
 
 }
