587eedcc32189bcd79d46ffb450d93de1b47a860
Youssef <youssefhad2@gmail.com>
2016-11-20 22:23:44 +0000

    add init function for validator and use langFolder

--- src/Validation/Validator.php
+++ src/Validation/Validator.php
@@ -2,21 +2,51 @@
 
 namespace Vinala\Kernel\Validation;
 
-use Valitron\Validator;
+use Valitron\Validator as V;
 
 /**
 * Validatior class
 */
 class Validator
 {
-
 	
+	//--------------------------------------------------------
+	// Properties
+	//--------------------------------------------------------
+
 	/**
 	* the Kint validator 
 	*
 	* @var Valitron\Validator
 	*/
 	protected static $validator ;
+
+	/**
+	* Translator file path
+	*
+	* @var string 
+	*/
+	protected static $langFolder ;
+	
+	//--------------------------------------------------------
+	// Functions
+	//--------------------------------------------------------
+
+	/**
+	* Initiate validator
+	*
+	* @return null
+	*/
+	public static function ini()
+	{
+		$lang = config('lang.default');
+
+		self::$langFolder = dirname(dirname(__DIR__))."/app/lang/".$lang;
+
+		V::langDir(self::$langFolder); 
+		V::lang('validator');
+	}
+	
 
 
 	
