84e9e797bf6d174ab71f59d4e96811b3711d1e93
Youssef <youssefhad2@gmail.com>
2016-11-27 13:23:45 +0000

    add event listener

---
+++ src/Event/EventListener.php
@@ -0,0 +1,34 @@
+<?php 
+
+namespace Vinala\Kernel\Events ;
+
+/**
+* 
+*/
+class EventListener
+{
+	
+	/**
+	* Set events pattern and thier function
+	*
+	* @var array
+	*/
+	protected static $events;
+
+
+	/**
+	* Get Events of Listener
+	*
+	* @param 
+	* @param 
+	* @return 
+	*/
+	public static function name()
+	{
+		
+		return ;
+	}
+	
+
+	
+}
