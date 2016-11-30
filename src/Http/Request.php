93fca701bab8b1eee45c22822a17aa72aa4e46e5
Youssef <youssefhad2@gmail.com>
2016-11-30 23:22:31 +0000

    add request class

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -8,4 +8,46 @@ namespace Vinala\Kernel\Http\Requests ;
 class Request
 {
 	
+
+	/**
+	* Data array contains request data
+	*
+	* @var array 
+	*/
+	protected $data = array() ;
+
+
+	function __construct()
+	{
+		$this->data = $this->getData();
+
+		$this->setProperties();
+	}
+
+
+	/**
+	* Get resuest data
+	*
+	* @return array
+	*/
+	protected function getData()
+	{
+		return $_REQUEST;
+	}
+
+	/**
+	* Set request data as class properties
+	*
+	* @return bool
+	*/
+	protected function setProperties()
+	{
+		foreach ($this->data as $key => $value) 
+		{
+			$this->$key = $value;
+		}
+	}
+	
+	
+
 }
