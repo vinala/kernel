32069de2922a4e594163347dcd79d2a7cf8cb0ea
Youssef <youssefhad2@gmail.com>
2016-12-03 23:39:55 +0000

    add Middleware class

---
+++ src/Http/Middleware/Middleware.php
@@ -0,0 +1,35 @@
+<?php 
+
+namespace Vinala\Kernel\Http\Middleware;
+
+/**
+* Middle ware class
+*/
+class Middleware
+{
+	
+	/**
+	* Run Middleware
+	*
+	* @param string $name
+	* @return bool
+	*/
+	public static function run($name)
+	{
+		
+		return ;
+	}
+
+	/**
+	* Get Middle ware
+	*
+	* @param string $name		
+	* @return string
+	*/
+	protected static function call($name)
+	{
+		return Filters::RoutesMiddleware($name);
+	}
+	
+	
+}
