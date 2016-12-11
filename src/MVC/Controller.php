777f287f8b4440d40df07ffe83297ce73e44454e
Youssef <youssefhad2@gmail.com>
2016-12-11 17:10:00 +0000

    add function called each to set all

--- src/MVC/Controller.php
+++ src/MVC/Controller.php
@@ -1,6 +1,6 @@
 <?php 
 
-namespace Vinala\Kernel\MVC\Controller;
+namespace Vinala\Kernel\MVC;
 
 use Vinala\Kernel\Http\Request;
 use Vinala\Kernel\Validation\Validator;
@@ -8,6 +8,10 @@ use Vinala\Kernel\MVC\ORM;
 
 /**
 * Controller class
+*
+* @version 1.0
+* @author Youssef Had
+* @package Vinala\Kernel\MVC
 */
 class Controller
 {
@@ -30,6 +34,35 @@ class Controller
 		return Validator::make($data , $rules);
 	}
 
+	/**
+	* Set each properties of model from request
+	*
+	* @param Vinala\Kernel\MVC\ORM $model
+	* @param Vinala\Kernel\Http\Request $request
+	* @param array $data
+	* @return Vinala\Kernel\MVC\ORM
+	*/
+	public static function each(ORM &$model , Request $request , $data = null)
+	{
+		if( ! is_null($data))
+		{
+			foreach ($data as $key) 
+			{
+				$model->$key = $request->$$key;
+			}
+		}
+		else
+		{
+			foreach ($request->all() as $key => $value) 
+			{
+				if(in_array($key, $model->_columns))
+				{
+					$model->$key = $request->$key;
+				}
+			}
+			di($model);
+		}
+	}
 	
 	
 	
