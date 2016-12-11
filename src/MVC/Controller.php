fe4ed13af54e8db87cfb02c0ddb433de26cddcfe
Youssef <youssefhad2@gmail.com>
2016-12-11 14:29:56 +0000

    add validate function for controller

--- src/MVC/Controller.php
+++ src/MVC/Controller.php
@@ -2,10 +2,35 @@
 
 namespace Vinala\Kernel\MVC\Controller;
 
+use Vinala\Kernel\Http\Request;
+use Vinala\Kernel\Validation\Validator;
+use Vinala\Kernel\MVC\ORM;
+
 /**
 * Controller class
 */
 class Controller
 {
+	
+	//--------------------------------------------------------
+	// Fucntions
+	//--------------------------------------------------------
+
+	/**
+	* Validate a request
+	*
+	* @param Vinala\Kernel\Http\Request $request
+	* @param array $rules
+	* @return bool
+	*/
+	public static function validate(Request $request , array $rules)
+	{
+		$data = $request->all();
+
+		return Validator::make($data , $rules);
+	}
+
+	
+	
 	
 }
