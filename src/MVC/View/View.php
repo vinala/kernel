bf3c9d0e096c59e079531dd02f3bd0a1ed8c21ac
Youssef <youssefhad2@gmail.com>
2016-12-11 13:10:46 +0000

    update make function and add show() and extend() functions

--- src/MVC/View/View.php
+++ src/MVC/View/View.php
@@ -1,18 +1,77 @@
 <?php
 
-namespace Vinala\Kernel\MVC\View;
+namespace Vinala\Kernel\MVC;
 
+use Vinala\Kernel\MVC\Views;
 /**
 * View mother class
 */
 class View
 {
 
+	//--------------------------------------------------------
+	// Properties
+	//--------------------------------------------------------
 
-	public static function make($value,$data=null)
+
+	/**
+	* The last view used by makefunction
+	*
+	* @var Vinala\Kernel\MVC\Views 
+	*/
+	protected static $view ;
+	
+
+	//--------------------------------------------------------
+	// Functions
+	//--------------------------------------------------------
+
+	/**
+	* Call and make a view
+	*
+	* @param string $name
+	* @param array $data
+	* @return Vinala\Kernel\MVC\Views
+	*/
+	public static function make( $name , $data = null)
 	{
-		Views::make($value,$data);
+		self::$view = new Views;
+
+		return self::$view->call($name , $data);
 	}
+
+	/**
+	* Show a view
+	*
+	* @param Vinala\Kernel\MVC\Views $view
+	* @return null
+	*/
+	public static function show(Views $view)
+	{
+		$v = new Views;
+
+		return $v->show($view);
+	}
+
+	/**
+	* Extend to child view
+	*
+	* @param string $name
+	* @param array $data
+	* @return 
+	*/
+	public static function extend($name , $data = null)
+	{
+		// d(self::$view);
+		return self::$view->call($name , $data)->show();
+	}
+	
+	
+	
+
+
+
+
 
 	public static function get($value,$data=null)
 	{
@@ -34,6 +93,9 @@ class View
 	{
 		return Views::exists($name);
 	}
+
+
+	
 	
 
 
