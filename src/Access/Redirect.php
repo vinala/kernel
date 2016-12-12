a3fdae1d755e077ffe37ca191c962d1bc0ddc891
Youssef <youssefhad2@gmail.com>
2016-12-12 13:42:47 +0000

    add surface for redirection

---
+++ src/Access/Redirect.php
@@ -0,0 +1,48 @@
+<?php 
+
+namespace Vinala\Kernel\Access ;
+
+//use SomeClass;
+
+/**
+* Redirect class
+*
+* @version 1.0
+* @author Youssef Had
+* @package Vinala\Kernel\Access
+* @since v3.3.0
+*/
+class Redirect 
+{
+	
+	//--------------------------------------------------------
+	// Properties
+	//--------------------------------------------------------
+
+	//
+
+	//--------------------------------------------------------
+	// Constructor
+	//--------------------------------------------------------
+
+	function __construct()
+	{
+		
+	}
+
+	//--------------------------------------------------------
+	// Functions
+	//--------------------------------------------------------
+
+	/**
+	* Redirect to previous location
+	*
+	* @return mixed
+	*/
+	public function back()
+	{
+		return header('Location: ' . $_SERVER['HTTP_REFERER']);
+	}
+	
+
+}
