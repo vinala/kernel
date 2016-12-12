ae27381ff4e19ce41b3fbcdd5acb84796594ea9b
Youssef <youssefhad2@gmail.com>
2016-12-12 13:41:43 +0000

    add class to deal with surfaces the new name for the framework component

---
+++ src/Surfaces/Surface.php
@@ -0,0 +1,57 @@
+<?php 
+
+namespace Vinala\Kernel\Surfaces ;
+
+//use SomeClass;
+
+/**
+* The main surfaces class
+*
+* @version 1.0
+* @author Youssef Had
+* @package Vinala\Kernel\Surfaces
+* @since 3.3.0
+*/
+class Surface
+{
+	
+	//--------------------------------------------------------
+	// Properties
+	//--------------------------------------------------------
+
+	/**
+	* The last Instance used bu Surface
+	*
+	* @var mixed 
+	*/
+	public static $instance;
+
+	//--------------------------------------------------------
+	// Constructor
+	//--------------------------------------------------------
+
+	function __construct()
+	{
+		
+	}
+
+	//--------------------------------------------------------
+	// Functions
+	//--------------------------------------------------------
+
+	/**
+	* Set Instance
+	*
+	* @param string $name
+	* @return mixed
+	*/
+	public static function setInstance($name)
+	{
+		$name = ucfirst($name);
+
+		static::$instance = new $name;
+
+		return static::$instance;
+	}
+
+}
