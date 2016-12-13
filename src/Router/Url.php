ae7edb5bc52ae313c7f255ac47ef76b8e7f6b61d
Youssef <youssefhad2@gmail.com>
2016-12-13 23:34:28 +0000

    add class for url

---
+++ src/Router/Url.php
@@ -0,0 +1,68 @@
+<?php 
+
+namespace Vianal\Kernel\Routing ;
+
+use Vinala\Kernel\Objects\Strings;
+
+/**
+* The URL genarator class
+*
+* @version 1.0
+* @author Youssef Had
+* @package Vianal\Kernel\Routing
+* @since v3.3.0
+*/
+class Url
+{
+	
+	//--------------------------------------------------------
+	// Properties
+	//--------------------------------------------------------
+
+	//
+
+	//--------------------------------------------------------
+	// Constructor
+	//--------------------------------------------------------
+
+	function __construct()
+	{
+		
+	}
+
+	//--------------------------------------------------------
+	// Functions
+	//--------------------------------------------------------
+
+	/**
+	* Genarte to Url 
+	*
+	* @param string $path
+	* @param array $extra
+	* @param bool $secure
+	* @return string
+	*/
+	public static function to($path , $extra = [] , $secure = null)
+	{
+		
+		return ;
+	}
+
+	/**
+	* Check if Url is valid
+	*
+	* @param string $path
+	* @return bool
+	*/
+	protected static function isValidUrl($path)
+	{
+		if(Strings::startsWith($path,['#', '//', 'mailto:', 'tel:', 'http://', 'https://'])) {
+            return true;
+        }
+
+        return filter_var($path, FILTER_VALIDATE_URL) !== false;
+	}
+	
+	
+
+}
