fbf8fd0f0c2cfb1226cf3e3d90e307fc56002e19
Youssef <youssefhad2@gmail.com>
2016-12-14 21:32:43 +0000

    create new redirector class

---
+++ src/Http/Redirect/Redirector.php
@@ -0,0 +1,59 @@
+<?php 
+
+namespace Vinala\Kernel\Access ;
+
+//use SomeClass;
+
+/**
+* Redirect class
+*
+* @version 1.0
+* @author Youssef Had
+* @package Vinala\Kernel\Access
+* @since v3.3.0
+*/
+class Redirect 
+{
+	
+	//--------------------------------------------------------
+	// Properties
+	//--------------------------------------------------------
+
+	//
+
+	//--------------------------------------------------------
+	// Constructor
+	//--------------------------------------------------------
+
+	function __construct()
+	{
+		
+	}
+
+	//--------------------------------------------------------
+	// Functions
+	//--------------------------------------------------------
+
+	/**
+	* Redirect to previous location
+	*
+	* @return mixed
+	*/
+	public function back()
+	{
+		return header('Location: javascript://history.go(-1)');
+	}
+	
+	/**
+	* Redirect to some url
+	*
+	* @param string $url
+	* @return mixed
+	*/
+	public function to($url)
+	{
+		return ;
+	}
+	
+
+}
