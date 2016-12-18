1715b3e4b64b353b00d6f251d205d79269118928
Youssef <youssefhad2@gmail.com>
2016-12-18 15:26:33 +0000

    add class for Redirect

---
+++ src/Http/Redirect/Redirect.php
@@ -0,0 +1,62 @@
+<?php 
+
+namespace Vinala\Kernel\Http\Redirect ;
+
+/**
+* Main redirect class
+*
+* @version 1.0
+* @author Youssef Had
+* @package Vinala\Kernel\Http\Redirect
+* @since v3.3.0
+*/
+class Redirect extends Redirector
+{
+
+	//--------------------------------------------------------
+	// Properties
+	//--------------------------------------------------------
+
+
+	/**
+	* Instance of Redirector class
+	*
+	* @var Vinala\Kernel\Http\Redirect\Redirector
+	*/
+	protected static $instance = null;
+	
+
+	//--------------------------------------------------------
+	// Functions
+	//--------------------------------------------------------
+
+	/**
+	* Get and set instance of Redirector
+	*	
+	* @return Vinala\Kernel\Http\Redirect\Redirector
+	*/
+	public static function getInstance()
+	{
+		if( ! is_null(self::$instance))
+		{
+			return self::$instance;
+		}
+
+		self::$instance = cube('redirector');
+
+		return self::$instance;
+	}
+	
+
+	/**
+	* Redirect back function
+	*
+	* @return bool
+	*/
+	public static function back()
+	{
+		return getInstance()->back();
+	}
+	
+
+}
