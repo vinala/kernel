e50f807ccbfd5e59011384c38c3b592b5e094547
Youssef <youssefhad2@gmail.com>
2016-12-19 22:20:04 +0000

    make class name first upper

--- src/Processes/Model.php
+++ src/Processes/Model.php
@@ -12,29 +12,30 @@ class Model
 {
 	public static function create($class , $table, $rt= null)
 	{
-		$file = $class;
+		$class = ucfirst($class);
 		
+		$file = $class;
+
 		$root = is_null($rt) ? Process::root : $rt ;
 
-		
 		if( ! file_exists($root."app/models/$file.php"))
-			{
-				$myfile = fopen($root."app/models/$file.php", "w");
-				$txt = self::set($class , $table);
+		{
+			$myfile = fopen($root."app/models/$file.php", "w");
+			$txt = self::set($class , $table);
 
-				fwrite($myfile, $txt);
-				fclose($myfile);
-				//
-				return true;
-			}
-			else return false;
+			fwrite($myfile, $txt);
+			fclose($myfile);
+			//
+			return true;
+		}
+		
+		return false;
 	}
 
 	public static function set($class , $table)
 	{
-		$txt = "<?php\n\namespace App\Model;\n\nuse Vinala\Kernel\MVC\ORM;\n\n";
+		$txt = "<?php\n\nnamespace App\Model;\n\nuse Vinala\Kernel\MVC\ORM;\n\n";
 		$txt.="class $class extends ORM\n{\n\t/**\n\t* Name of the DataTable\n\t*/\n\tpublic static ".'$_table'."='$table';\n\n}";
-		// $txt.="class $class extends ORM\n{\n\t//Name of the table in database\n\tpublic static ".'$table'."='$table';\n\tprotected static ".'$foreignKeys=array();'."\n\n}";
 		//
 		return $txt;
 	}
