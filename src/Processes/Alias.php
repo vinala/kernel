10175f373f679f7c3af638a54c4f98fd6a2c2e87
Youssef <youssefhad2@gmail.com>
2016-12-20 23:20:53 +0000

    update format function

--- src/Processes/Alias.php
+++ src/Processes/Alias.php
@@ -27,9 +27,9 @@ class Alias
 	protected static function docs()
 	{
 		return [
-			'enable' => "\n\t/*\n\t|----------------------------------------------------------\n\t| Enable Aliases\n\t|----------------------------------------------------------\n\t| Here to activate classes aliases\n\t|\n\t**/",
+			'enable' => "\n\t/*\n\t|----------------------------------------------------------\n\t| Enable Aliases\n\t|----------------------------------------------------------\n\t| Here to activate classes aliases\n\t|\n\t**/\n",
 
-			'kernel' => "\n\t/*\n\t|----------------------------------------------------------\n\t| Kernel Aliases\n\t|----------------------------------------------------------\n\t| this array is responsible for aliases of class\n\t| in the kernel.\n\t|\n\t**/",
+			'kernel' => "\n\t/*\n\t|----------------------------------------------------------\n\t| Kernel Aliases\n\t|----------------------------------------------------------\n\t| this array is responsible for aliases of class\n\t| in the kernel.\n\t|\n\t**/\n",
 
 			'user' => "\n\t/*\n\t|----------------------------------------------------------\n\t| User Aliases\n\t|----------------------------------------------------------\n\t| this array is responsible for your aliases, feel\n\t| free to register as many as \n\t| you wish as the aliases are 'lazy' loaded so \n\t| they don't hinder performance.\n\t|\n\t**/",
 
@@ -40,6 +40,55 @@ class Alias
 			'models' => "\n\t/*\n\t|----------------------------------------------------------\n\t| Models Aliases\n\t|----------------------------------------------------------\n\t| this array is responsible for aliases of models class\n\t| classes\n\t|\n\t**/",
 		];
 	}
+
+	/**
+	* Set the config params
+	*
+	* @param bool $enabled
+	* @param array $params
+	* @return bool
+	*/
+	public static function set($enabled , $params)
+	{
+		$docs = self::docs();
+
+		$enabled = $docs['enabled'].self::enbaledFormat($enabled);
+		$kernel = $docs['kernel'].self::kernelFormat($params['kernel']);
+
+		return true;
+	}
+
+	/**
+	* Enabled switch format
+	*
+	* @param bool $enabled
+	* @return string
+	*/
+	protected static function enbaledFormat($enabled)
+	{
+		return "'enable' => ".($enabled ? 'true' : 'false')." ,";
+	}
+
+	/**
+	* Kernel array format
+	*
+	* @param array $data
+	* @return string
+	*/
+	protected static function kernelFormat($data)
+	{
+		$format = "\t'kernel' => [\n";
+
+		foreach ($data as $key => $value) {
+			$format .= "\t\t'$key' => $value::class , \n";
+		}
+
+		$format .= "\t],";
+
+		return $format;
+	}
+	
+	
 	
 
 }
