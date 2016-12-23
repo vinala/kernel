1b3c867a50dd6e8e71852dc5ad38b64b8a8d409c
Youssef <youssefhad2@gmail.com>
2016-12-23 22:46:58 +0000

    add class for the classed that will be aliased if not found

---
+++ src/Config/Exceptions/AliasedClassNotFoundException.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace Vinala\Kernel\Config\Exceptions;
+
+use Vinala\Kernel\Logging\Exception;
+
+/**
+* Classv Aliased Not Found Exception
+*/
+class AliasedClassNotFoundException extends Exception
+{
+
+	//--------------------------------------------------------
+	// Constructor
+	//--------------------------------------------------------
+	function __construct($class , $array) 
+	{
+		$this->message = "The class '$class' in '$array' aliases not found";
+	}
+}
