9027ced44f8e39b475a916804f5e3dcac942af84
Youssef <youssefhad2@gmail.com>
2016-12-29 11:11:40 +0000

    use connector namepsace

--- src/Setup/Assets/Views/main.php
+++ src/Setup/Assets/Views/main.php
@@ -8,14 +8,11 @@
 		use Vinala\Kernel\Config\Config;
 		use Vinala\Kernel\Translator\Lang;
 		use Vinala\Kernel\Foundation\Application;
+		use Vinala\Kernel\Foundation\Connector;
 		//
 		Libs::css("app/resources/library/bootstrap-3.3.1.min.css",false);
 		Libs::css("app/resources/library/bootstrap-theme-3.3.1.min.css",false);
 		Libs::js("app/resources/library/jquery-1.11.3.min.js",false);
-		//
-		// Libs::css(Application::$root."vendor/vinala/kernel/src/Setup/Assets/bootstrap-3.3.1.min.css",false);
-		// Libs::css(Application::$root."vendor/vinala/kernel/src/Setup/Assets/bootstrap-theme-3.3.1.min.css",false);
-		// Libs::js(Application::$root."vendor/vinala/kernel/src/Setup/Assets/jquery-1.11.3.min.js",false);
 		//
 		Html::charset("utf-8"); 
 		Html::title();
