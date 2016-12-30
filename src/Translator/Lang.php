955a439714e81e91f0339339647bf7f8b3f88b16
Youssef <youssefhad2@gmail.com>
2016-12-30 13:56:22 +0000

    huge update Lang cube to set v1.1 of the cube

--- src/Translator/Lang.php
+++ src/Translator/Lang.php
@@ -3,142 +3,251 @@
 namespace Vinala\Kernel\Translator;
 
 use Vinala\Kernel\Translator\Exception\LanguageKeyNotFoundException;
-use Vinala\Kernel\Config\Config;
 use Vinala\Kernel\Storage\Cookie;
-use Vinala\Kernel\HyperText\Res;
-use Vinala\Kernel\Objects\Base;
-use Vinala\Kernel\Filesystem\Filesystem;
-use Vinala\Kernel\Foundation\Application;
+use Vinala\Kernel\Storage\Session;
+use Vinala\Kernel\Filesystem\File;
+
 
 /**
-* Language class
+* Translator class
+*
+* @version v1.1
+* @author Youssef Had
+* @package 
+* @since v1.0
 */
 class Lang
 {
-	static $textes;
-	public static $lang;
-	private static $supportedLangs=array();
+	
+	//--------------------------------------------------------
+	// Properties
+	//--------------------------------------------------------
 
-	public static function ini($lang=null)
+	
+	/**
+	* Words used in current language
+	*
+	* @var array 
+	*/
+	protected static $words = [] ;
+
+
+	/**
+	* Selected language
+	*
+	* @var string 
+	*/
+	public static $lang = null ;
+
+
+	/**
+	* Supported languages
+	*
+	* @var array 
+	*/
+	protected static $supported = [] ;
+
+
+	/**
+	* Hashed name of lang cookie
+	*
+	* @var string 
+	*/
+	protected static $cookieName = null ;
+
+
+	/**
+	* Lang session name
+	*
+	* @var string 
+	*/
+	protected static $sessionName = 'Vinala_lang' ;
+	
+
+	//--------------------------------------------------------
+	// Constructor
+	//--------------------------------------------------------
+
+	function __construct()
 	{
-		self::$supportedLangs=self::getSupported();
 		//
-		self::$lang= is_null($lang) ? self::detect() : $lang ;
-		self::put();
 	}
 
-	public static function get($key)
+	//--------------------------------------------------------
+	// Functions
+	//--------------------------------------------------------
+
+	/**
+	* Initiate the Lang class
+	*
+	* @param string $lang
+	* @return null
+	*/
+	public static function ini($lang = null)
 	{
-		$value = "";
-		//
-		if(array_has(self::$textes , $key)) 
-			{
-				$value = array_get(self::$textes , $key);
-			}
-		else throw new  LanguageKeyNotFoundException($key);
-		//
-		return $value;
+		self::getSupported();
+		
+		self::$lang = is_null($lang) ? self::detect() : $lang ;
+		
+		self::load();
 	}
 
-	public static function set($lang)
+	/**
+	* Get supported langs used by the app
+	*
+	* @return array
+	*/
+	protected static function getSupported()
 	{
-		Cookie::create(self::getName(),$lang,(60*24*7*30));
-		Res::stsession("Lighty_lang",$lang);
-	}
+		$folders = File::directories(root().'resources/translator');
 
-	private static function put()
-	{
-		foreach (glob(Application::$root."resources/translator/".self::$lang."/*.php") as $filename)
+		foreach ($folders as $folder) 
 		{
-			$tbl=(need($filename));
-			foreach ($tbl as $key => $value) {
-				self::$textes[$key]=$value;
-			}
+			$lang = explode(root() . 'resources/translator/', $folder);
+
+			self::$supported[] = $lang[1];
 		}
 	}
 
+	/**
+	* Detect the used language
+	*
+	* @return string
+	*/
 	public static function detect()
 	{
+		$key = self::$sessionName;
 
-		if(isset($_SESSION["Lighty_lang"]) && !empty($_SESSION["Lighty_lang"]))
-		{
-			if (in_array($_SESSION["Lighty_lang"], self::$supportedLangs)) 
-			{
+		$cookieName = self::cookieName($key);
 
-			    if(Base::full(self::getCookie())) 
-			    {
-			    	if(self::getCookie() != $_SESSION["Lighty_lang"])
-			    	{
-			    		Cookie::create(self::getName(),$_SESSION["Lighty_lang"],(60*24*7));
-			    	}
-			    }
-			    else  
-			    {
-			    	Cookie::create(self::getName(),$_SESSION["Lighty_lang"],(60*24*7));
-			    }
-			}
-			else 
-			{			
-				Res::stsession("Lighty_lang",Config::get('lang.default'));
-				if(Base::full(self::getCookie())) 
-			    	if(self::getCookie() != Config::get('lang.default'))
-			    	{
-						Cookie::create(self::getName(),Config::get('lang.default'),(60*24*7));				
-			    	}
-			}
-		}
-		else if(Base::full(self::getCookie()))
+		// check if the session lang exists
+		if(Session::existe($key))
 		{
-			if(in_array(self::getCookie(), self::$supportedLangs))
+		
+			//check if supported languages contains the session lang value 
+			if( ! in_array(Session::get($key) , self::$supported))
 			{
-				Res::stsession("Lighty_lang",self::getCookie());
+				Session::put($key , config('lang.default' , 'en'));
+			}
+
+			if(Cookie::existe($cookieName))
+			{
+				if(Cookie::get($cookieName) != Session::get($key))
+				{
+					Cookie::create($cookieName , Session::get($key) , (60*24*7));
+				}
 			}
 			else
 			{
-				Cookie::create(self::getName(),Config::get('lang.default'),(60*24*7));
-				Res::stsession("Lighty_lang",Config::get('lang.default'));
+				Cookie::create($cookieName , Session::get($key) , (60*24*7));
 			}
 		}
-		else 
+
+		// check if the cookie lang exists
+		elseif(Cookie::existe($cookieName))
 		{
-			Res::stsession("Lighty_lang",Config::get('lang.default'));
+			//check if supported languages contains the cookie lang value 
+			if( in_array(Cookie::get($key) , self::$supported))
+			{
+				Session::put($key , Cookie::get($key) );
+			}
+			else
+			{
+				Session::put($key , config('lang.default' , 'en'));
+				Cookie::create($cookieName , config('lang.default' , 'en') , (60*24*7));
+			}
 		}
-		//
-		return Res::session("Lighty_lang");
-	}
 
-	private static function hash($value)
-	{
-		return md5(md5($value)."lang".Config::get('security.key1').md5(Config::get('security.key2')));
-	}
-
-	private static function getName()
-	{
-		return self::hash(Config::get('lang.cookie'));
-	}
-
-	private static function getCookie()
-	{
-		return Cookie::get(self::getName());
-	}
-
-	private static function getSupported()
-	{
-		$supp=array();
-		$sup=(new Filesystem)->directories(Application::$root."resources/translator");
-		//
-		foreach ($sup as $value) {
-			$r=explode(Application::$root."resources/translator/", $value);
-			$supp[]=$r[1];
+		// if cokkie and session not found
+		else
+		{
+			Session::put($key , config('lang.default' , 'en'));
+			Cookie::create($cookieName , config('lang.default' , 'en') , (60*24*7));
 		}
-		//
-		return $supp;
+
+		return Session::get($key);
 	}
 
+	/**
+	* Set the cookie name
+	*
+	* @param string $name
+	* @return string
+	*/
+	protected static function cookieName($name)
+	{
+		return self::$cookieName = self::hash($name);
+	}
+
+
+	/**
+	* Hash the name of the lang cookie
+	*
+	* @param string $value
+	* @return string
+	*/
+	protected static function hash($value)
+	{
+		return md5(md5($value)."lang".config('security.key1').md5(config('security.key2')));
+	}
+
+
+	/**
+	* Get all words in supported language
+	*
+	* @return null
+	*/
+	protected static function load()
+	{
+		$files = glob(root().'resources/translator/'.self::$lang.'/*.php');
+
+		foreach ($files as $file) 
+		{
+			$words = need($file);
+
+			foreach ($words as $key => $value) 
+			{
+				self::$words[$key] = $value ;
+			}
+		}
+	}
+
+	/**
+	* Change tha languauge used
+	*
+	* @param string $lang
+	* @return true
+	*/
+	public static function set($lang)
+	{
+		Cookie::create(self::$cookieName,$lang,(60*24*7*30));
+		Session::put(self::$sessionName , $lang);
+		return true;
+	}
+
+
+	/**
+	* Get the translate word by a key
+	*
+	* @param string $key
+	* @return string
+	*/
+	public static function get($key)
+	{
+		exception_if( ! array_has(self::$words , $key) , LanguageKeyNotFoundException::class , $key);
+		
+		return array_get(self::$words , $key);	
+	}
+
+
+	/**
+	* Set the default language
+	*
+	* @return true
+	*/
 	public static function setDefault()
 	{
-		Cookie::create(self::getName(),Config::get('lang.default'),(60*24*7));
-		Res::stsession("Lighty_lang",Config::get('lang.default'));
+		return self::set(config('lang.default'));
 	}
 	
 }
