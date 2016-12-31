01205fee403983e1cf9fb11ece5302de02b54445
Youssef <youssefhad2@gmail.com>
2016-12-31 14:14:50 +0000

    add exception for not supported languages

---
+++ src/Translator/Exceptions/LanguageNotSupportedException.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace Vinala\Kernel\Translator\Exception;
+
+use Vinala\Kernel\Logging\Exception;
+
+/**
+* Language not supported exception
+*/
+class LanguageNotSupportedException extends Exception
+{
+
+	//--------------------------------------------------------
+	// Constructor
+	//--------------------------------------------------------
+
+	function __construct($lang) 
+	{
+		$this->message = 'The language "'.$lang.'" is not supported by '. ( empty(config('app.project')) ? 'the application' : config('app.project'));
+	}
+}
