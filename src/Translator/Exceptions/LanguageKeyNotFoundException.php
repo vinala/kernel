f866adc3451b023094442451b1c64dd4e7ac6722
Youssef <youssefhad2@gmail.com>
2016-12-31 12:38:04 +0000

    make the class extends from the framework exception class and use regular error as exception view

--- src/Translator/Exceptions/LanguageKeyNotFoundException.php
+++ src/Translator/Exceptions/LanguageKeyNotFoundException.php
@@ -2,15 +2,30 @@
 
 namespace Vinala\Kernel\Translator\Exception;
 
+use Vinala\Kernel\Logging\Exception;
+
 /**
 * Directory not fount exception
 */
-class LanguageKeyNotFoundException extends \Exception
+class LanguageKeyNotFoundException extends Exception
 {
-	protected $message = "The language Key you called not found";   // exception message
 
-	function __construct($key=null) {
-		if(is_null($key)) $this->message="The language Key you called not found";
-		else $this->message="There's no key language called '$key'";
+	//--------------------------------------------------------
+	// Constructor
+	//--------------------------------------------------------
+
+	function __construct($key = null) 
+	{
+		if(is_null($key)) 
+		{
+			$this->message = "The language Key you called not found";
+		}
+		else
+		{
+			$this->message = "There's no key language called '$key'";
+		}
+
+		$this->view = config('error.regular');
+
 	}
 }
