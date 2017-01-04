ee1624e2a9412971a59bd5457877f912342284a3
Youssef <youssefhad2@gmail.com>
2017-01-04 23:14:52 +0000

    update Loggin setup

--- src/Setup/Documentations/Loggin.php
+++ src/Setup/Documentations/Loggin.php
@@ -7,22 +7,19 @@ class Loggin
 	protected static function logginDoc($index)
 	{
 		$doc = array(
-			'debug' => "\n\t|  Here to make the framework shows errors and\n\t|  exceptions, false to show friendly messages\n\t|  and true to debug", 
-			'error_debug_message' => "\n\t|  If loggin.debug was false the framework will\n\t|  show this message",
-			'error_log' => "\n\t|  The path of log file where Lighty store errors\n\t|  by default the framework use this path \n\t|  'app/storage/logs/lighty.log'",
-			'background' => "\n\t|  The color background of simple page error"
+			'debug' => "\n\t|  Here to make the framework shows errors and\n\t|  exceptions, false to show friendly messages\n\t|  and true to debug\n\t|", 
+
+			'error_log' => "\n\t|  The path of log file where Lighty store errors\n\t|",
 			);
 		//
-		return $doc[$index]."\n\t*/";
+		return $doc[$index]."\n\t**/";
 	}
 
 	protected static function logginTitles($index)
 	{
 		$titles = array(
 			'debug' => "Allow Debug", 
-			'error_debug_message' => "Error Debug Message",
 			'error_log' => "Error log",
-			'background' => "Error simple page background color",
 			);
 		//
 		$sep = "\n\t|----------------------------------------------------------";
@@ -37,14 +34,12 @@ class Loggin
 		return $title.$doc."\n\n\t$param\n";
 	}
 
-	public static function set($loggin , $log ='app/storage/logs/lighty.log')
+	public static function set($loggin , $log ='storage/log/vinala.log')
 	{
 		$loggin = $loggin ? "true" : "false" ;
-		$debug = self::logginRow("debug","'debug'=>$loggin,");
-		$error_debug_message = self::logginRow("error_debug_message","'msg' => \"Ohlala! il semble que quelque chose s'ait mal passé\",");
-		$error_log = self::logginRow("error_log","'log' => '$log',");
-		$background = self::logginRow("background","'bg' => '#a4003a',");
+		$debug = self::logginRow("debug","'debug' => $loggin ,");
+		$error_log = self::logginRow("error_log","'log' => '$log' ,");
 		//
-		return "<?php \n\nreturn array(\n\t".$debug.$error_debug_message.$error_log.$background."\n);";
+		return "<?php\n\nreturn [\n\n\t".$debug.$error_log."\n\n];";
 	}
 }
