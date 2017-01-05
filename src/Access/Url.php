75eaa40f708f9a11ecfd3df816b211709d428d28
Youssef <youssefhad2@gmail.com>
2017-01-05 23:59:44 +0000

    add function to get root url

--- src/Access/Url.php
+++ src/Access/Url.php
@@ -4,6 +4,7 @@ namespace Vinala\Kernel\Access;
 
 use Vinala\Kernel\Config\Config;
 use Vinala\Kernel\Foundation\Application;
+use Vinala\Kernel\Filesystem\File;
 
 /**
 * Url class
@@ -36,4 +37,35 @@ class Url
 		self::$js=Application::$root."app/resources/js/";
 		self::$img=Application::$root."app/resources/images/";
 	}
+
+
+	/**
+	* Get the route url of the app
+	*
+	* @return string
+	*/
+	public static function root()
+	{
+		$url = request('REQUEST_URI' , '' , 'server');
+		$root = request('DOCUMENT_ROOT' , '' , 'server');
+
+		$url = substr($url, 1);
+
+		$parts = explode("/", $url);
+		//
+		$folder = "";
+		//
+		for ($i=0; $i < count($parts) ; $i++) 
+		{
+			if( File::isDirectory($root.$folder.$parts[$i]."/")) 
+			{
+				$folder .= $parts[$i]."/";
+			}
+			else
+			{
+				return $folder;
+			}
+		}
+	}
+	
 }
