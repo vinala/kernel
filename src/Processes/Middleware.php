c2c433aaf5eea458baaf6c17533b4ea28c23d690
Youssef <youssefhad2@gmail.com>
2017-01-06 22:42:04 +0000

    create the middleware process

---
+++ src/Processes/Middleware.php
@@ -0,0 +1,70 @@
+<?php 
+
+namespace Vinala\Kernel\Process ;
+
+use Vinala\Kernel\Process\Process;
+use Vinala\Kernel\Filesystem\File;
+
+/**
+* Documentation
+*
+* @version 1.0
+* @author Youssef Had
+* @package Vinala\Kernel\Process
+* @since v3.3.0
+*/
+class Middleware 
+{
+
+	//--------------------------------------------------------
+	// Functions
+	//--------------------------------------------------------
+
+	/**
+	* Function to create Middleware
+	*
+	* @param string $name
+	* @return bool
+	*/
+	public static function create($name)
+	{
+		$root = Process::root;
+
+		$path = $root."app/http/middleware/$name.php";
+
+		if( ! File::exists($path))
+		{
+			File::put($path , self::set($name));
+
+			return true;
+		}
+
+		return false;
+	}
+
+
+	/**
+	* Build the middleware script
+	*
+	* @param string $name
+	* @return string
+	*/
+	protected static function set($name)
+	{
+		$txt = "<?php\n\n";
+		$txt .= "namespace App\Http\Middleware;\n\n";
+		$txt .= "use Vinala\Kernel\Http\Request;\n\n";
+		$txt .= "/**\n* ".$name." Middleware\n*\n* @author ".config('app.owner')."\n**/\n";
+		$txt .= "class $name\n{\n\n";
+		$txt .= "\t/**\n\t* Handle the middleware\n";
+		$txt .= "\t*\n\t* @param Vinala\Kernel\Http\Request \$req\n";
+		$txt .= "\t* @return bool|string\n\t**/\n";
+		$txt .= "\tpublic function handle(Request \$req)\n\t{";
+		$txt .= "\n\t\t// ";
+		$txt .= "\n\t}\n\n}";
+
+		return $txt;
+	}
+	
+
+}
