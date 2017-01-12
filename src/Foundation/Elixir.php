dfd56e309346ea96ed2007d8debe1db68ac5fa58
Youssef <youssefhad2@gmail.com>
2017-01-12 00:06:04 +0000

    create elixir surface

---
+++ src/Foundation/Elixir.php
@@ -0,0 +1,56 @@
+<?php 
+
+namespace Vinala\Kernel\Foundation ;
+
+//use SomeClass;
+
+/**
+* The Elixir of the framework
+*
+* @version 1.0
+* @author Youssef Had
+* @package Vinala\Kernel\Foundation
+* @since v3.3.0
+*/
+class Elixir
+{
+	
+	//--------------------------------------------------------
+	// Properties
+	//--------------------------------------------------------
+
+	
+	/**
+	* The Elixir SHA
+	*
+	* @var string 
+	*/
+	private static $_SHA = "" ;
+	
+
+	//--------------------------------------------------------
+	// Constructor
+	//--------------------------------------------------------
+
+	function __construct()
+	{
+		//
+	}
+
+	//--------------------------------------------------------
+	// Functions
+	//--------------------------------------------------------
+
+	/**
+	* Check if Elixir allowed
+	*
+	* @return bool
+	*/
+	protected static function check()
+	{
+		if(self::remote() != self::$_SHA) return false;
+		return true;
+	}
+	
+
+}
