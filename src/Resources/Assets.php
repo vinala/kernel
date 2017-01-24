816d012209b337cafb4ad3b8c9f502040873177b
Youssef <youssefhad2@gmail.com>
2017-01-24 18:07:50 +0000

    update Assets surface to 1.1.0 version

--- src/Resources/Assets.php
+++ src/Resources/Assets.php
@@ -5,48 +5,145 @@ namespace Vinala\Kernel\Resources;
 use Vinala\Kernel\Access\Path;
 
 /**
+* The web assets surface
 *
+* @version 1.1.0
+* @author Youssef Had
+* @package Vinala\Kernel\Resources
+* @since v1.0.0
 */
 class Assets
 {
-	public static function css($file,$default=true)
+
+	//--------------------------------------------------------
+	// Functions
+	//--------------------------------------------------------
+
+	/**
+	* Call CSS files
+	*
+	* @param string|array $files
+	* @param bool $nest
+	* @return string
+	*/
+	public static function css( $files , $nest = true)
 	{
-		if (strpos($file,'http://') !== false) {
-		    echo '<link rel="stylesheet" type="text/css" href="'.$file.'.css">'."\n";
-		}
-		else
+		if(is_array($files))
 		{
-			if($default)
+			foreach ($files as $file) 
 			{
-				$file=str_replace('.', '/', $file);
-				// echo '<link rel="stylesheet" type="text/css" href="'.Path::$app.'/resources/css/'.$file.'.css">'."\n";
-				echo '<link rel="stylesheet" type="text/css" href="assets/css/'.$file.'.css">'."\n";
+				self::cssCall($file , $nest);
 			}
-			else
-			{
-				echo '<link rel="stylesheet" type="text/css" href="'.$file.'">'."\n";
-			}
+		}
+		elseif(is_string($files))
+		{
+			self::cssCall($files , $nest);
 		}
 	}
 
-	public static function js($file,$default=true)
+	/**
+	* The process to CSS HTML Tag
+	*
+	* @param string $file
+	* @param bool $nest
+	* @return null
+	*/
+	public static function cssCall($file , $nest)
 	{
-		if (strpos($file,'http://') !== false) {
-		    echo '<script type="text/javascript"  src="'.$file.'"></script>'."\n";
+		if (strpos($file,'http://') !== false) 
+		{
+		    $path = $file.'.css';
 		}
 		else
 		{
-
-			if($default)
+			if($nest)
 			{
-				$file=str_replace('.', '/', $file);
-				echo '<script type="text/javascript"  src="assets/js/'.$file.'.js"></script>'."\n";
+				$file = str_replace('.', '/', $file);
+				$path = 'assets/css/'.$file.'.css';
 			}
 			else
 			{
-				echo '<script type="text/javascript"  src="'.$file.'"></script>'."\n";
+				$path = $file;
 			}
 		}
 
+		self::cssTag($path);
 	}
+	
+
+	/**
+	* CSS call
+	*
+	* @param string $path
+	* @return null
+	*/
+	private static function cssTag($path)
+	{
+		echo '<link rel="stylesheet" type="text/css" href="'.$path.'">'."\n";
+	}
+
+
+	/**
+	* Call JS files
+	*
+	* @param string|array $files
+	* @param bool $nest
+	* @return string
+	*/
+	public static function js( $files , $nest = true)
+	{
+		if(is_array($files))
+		{
+			foreach ($files as $file) 
+			{
+				self::jsCall($file , $nest);
+			}
+		}
+		elseif(is_string($files))
+		{
+			self::jsCall($files , $nest);
+		}
+	}
+
+	/**
+	* The process to JS HTML Tag
+	*
+	* @param string $file
+	* @param bool $nest
+	* @return null
+	*/
+	public static function jsCall($file , $nest)
+	{
+		if (strpos($file,'http://') !== false) 
+		{
+		    $path = $file.'.js';
+		}
+		else
+		{
+			if($nest)
+			{
+				$file = str_replace('.', '/', $file);
+				$path = 'assets/js/'.$file.'.js';
+			}
+			else
+			{
+				$path = $file;
+			}
+		}
+
+		self::jsTag($path);
+	}
+	
+
+	/**
+	* JS call
+	*
+	* @param string $path
+	* @return null
+	*/
+	private static function jsTag($path)
+	{
+		echo '<script type="text/javascript"  src="'.$path.'"></script>'."\n";
+	}
+
 }
