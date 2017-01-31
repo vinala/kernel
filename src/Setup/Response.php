a2d21dec672e192242e5e7a2c44496e473b16a5c
Youssef <youssefhad2@gmail.com>
2017-01-31 17:57:14 +0000

    update the setup response to  deal  with the new setup view

--- src/Setup/Response.php
+++ src/Setup/Response.php
@@ -16,57 +16,8 @@ use Vinala\Kernel\Setup\Documentations\Robots;
 /**
 * class de controller helloController
 */
-
 class Response
 {
-	/**
-	 * get Database post data
-	 */
-	public static function setDb_step()
-	{
-		$host = $_POST['db_host'] ;
-		$name = $_POST['db_name'];
-		$usr = $_POST['db_usr'];
-		$pass = $_POST['db_pass'];
-		$prefix = $_POST['db_prefix'];
-		//
-		if( ! self::checkDb_step($host,$name,$usr,$pass,$prefix)) 
-		{
-			echo "false";
-			die();
-		}
-		else
-		{
-			self::makeDb_step($host,$name,$usr,$pass,$prefix);
-		}
-	}
-
-	/**
-	 * Check if database existe
-	 */
-	protected static function checkDb_step($host,$name,$usr,$pass,$prefix)
-	{
-		if(@mysqli_connect($host,$usr,$pass,$name)) return true;
-		else return false;
-	}
-
-	/**
-	 * set data in database file
-	 */
-	protected static function makeDb_step($host,$name,$usr,$pass,$prefix)
-	{
-		if(empty($prefix)) { $prefixing="false"; $prefix="ysf"; }
-		else  { $prefixing="true";  }
-		//
-		if( ! Application::$isTest) 
-		{
-			$content = Database::set($host,$name,$usr,$pass,$prefixing,$prefix);
-			//
-			file_put_contents(Application::$root."config/database.php", $content , 0);
-			//
-			echo "true";
-		}
-	}
 
 	/**
 	 * set global setup
@@ -88,7 +39,7 @@ class Response
 		//
 		if( ! Application::$isTest)
 		{
-			$appCont = App::set($name, $project);
+			$appCont = App::set($name, $project , true );
 			$translatorCont = Translator::set($langue);
 			$logginCont = Loggin::set($loggin);
 			Robots::set($robot);
@@ -98,44 +49,6 @@ class Response
 			file_put_contents(Application::$root."config/loggin.php", $logginCont, 0);
 			//
 			return true;
-		}
-	}
-
-	/**
-	 * set security setup
-	 */
-	public static function setSecur_step($sec_1 , $sec_2)
-	{
-		if( ! Application::$isTest) 
-		{
-			$content = Security::set($sec_1,$sec_2);
-			//
-			file_put_contents(Application::$root."config/security.php", $content , 0);
-			//
-			return true;
-		}
-	}
-
-	/**
-	 * set panel setup
-	 */
-	public static function setPanel_step()
-	{
-		if(isset($_POST['stat'])) $state="true";		
-		else $state="false";		
-		//		
-		$route=empty($_POST['route']) ? "lighty" : $_POST['route'];		
-		$pass_1=empty($_POST['pass_1']) ? "1234" : $_POST['pass_1'];		
-		$pass_2=empty($_POST['pass_2']) ? "5678" : $_POST['pass_2'];		
-		//
-		if( ! Application::$isTest) 
-		{
-			$state = $state ? "true" : "false" ;
-			$content = Panel::set($state,$route,$pass_1,$pass_2);
-			//
-			file_put_contents(Application::$root."config/panel.php", $content, 0);
-			//
-			echo "true";
 		}
 	}
 }
