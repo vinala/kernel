17ca9d02ef1ff57bde1d608aeb73f1ecf9ef4166
Youssef <youssefhad2@gmail.com>
2017-01-31 17:57:38 +0000

    use only one route

--- src/Setup/Routes.php
+++ src/Setup/Routes.php
@@ -2,17 +2,6 @@
 
 use Vinala\Kernel\Setup\Response;
 
-// get("hello/db_check",function() { return Response::setDb_step(); });
-
-
-
 get("hello/setup",function() { 
-	if(Response::setGlob_step()) 
-		return Response::setPanel_step();
-	else echo 'false';
+	Response::setGlob_step();
 });
-
-
-
-// get("hello/set_secur",function() { return Response::setSecur_step(); });
-// get("hello/set_panel",function() { return Response::setPanel_step(); });
