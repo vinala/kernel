7f51cbe36c44fc1c291ce7d3bd20537244000ae7
Youssef <youssefhad2@gmail.com>
2017-02-11 22:11:08 +0000

    make exception for not founded model

---
+++ src/Authentication/Exceptions/AuthenticationModelNotFoundException.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace Vinala\Kernel\Authentication\Exceptions;
+
+use Vinala\Kernel\Logging\Exception;
+
+/**
+* Authentication Fields Not Found Exception
+*/
+class AuthenticationModelNotFoundException extends Exception
+{
+
+	//--------------------------------------------------------
+	// Constructor
+	//--------------------------------------------------------
+	function __construct() 
+	{
+		$this->message = 'The model used in authentication config not found';
+		
+		$this->view = config('error.regular');
+	}
+}
