6250db0758ff17c74357e37c07bcd9961e704069
Youssef <youssefhad2@gmail.com>
2017-02-12 16:27:38 +0000

    add some function to cache main driver

--- src/Caches/Drivers/Driver.php
+++ src/Caches/Drivers/Driver.php
@@ -41,6 +41,17 @@ class Driver
 	//--------------------------------------------------------
 
 	/**
+	* Set the cache item
+	*
+	* @param string $key
+	* @return Symfony\Component\Cache\CacheItem
+	*/
+	private function set($key)
+	{
+		return static::$adapter->getItem($key);
+	}
+	
+	/**
 	* Get the cache value by the key name
 	*
 	* @param string $key
@@ -49,7 +60,7 @@ class Driver
 	*/
 	public function get($key , $default = null)
 	{
-		$item = static::$adapter->getItem($key);
+		$item = $this->set($key);
 
 		if ($item->isHit()) 
 		{
@@ -68,7 +79,7 @@ class Driver
 	*/
 	public function put($key  , $value)
 	{
-		$item = static::$adapter->getItem($key);
+		$item = $this->set($key);
 
 		$item->set($value);
 
@@ -78,19 +89,46 @@ class Driver
 	}
 
 	/**
-	* Check if there is a key in cache
+	* Check if there is an item in cache
 	*
 	* @param string $key
 	* @return bool
 	*/
 	public function has($key)
 	{
-		$item = static::$adapter->getItem($key);
+		$item = $this->set($key);
 
 		return $item->isHit();
 	}
 
+	/**
+	* Remove a cache item
+	*
+	* @param string $key
+	* @return bool
+	*/
+	public function remove($key)
+	{
+		return static::$adapter->deleteItem($key);
+	}
 
+	/**
+	* Get an item cache and remove it
+	*
+	* @param string $key
+	* @return mixed
+	*/
+	public function pull($key)
+	{
+		$item = $this->get($key);
+
+		$this->remove($key);
+
+		return $item;
+	}
+	
+	
+	
 	
 	
 
