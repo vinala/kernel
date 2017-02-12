1d84071cd00e672451674e30028cf21f5e13adbe
Youssef <youssefhad2@gmail.com>
2017-02-12 23:17:38 +0000

    create class for cache items

---
+++ src/Caches/Item.php
@@ -0,0 +1,68 @@
+<?php 
+
+namespace Vinala\Kernel\Cache ;
+
+use Symfony\Component\Cache\CacheItem;
+
+/**
+* The item cache class
+*
+* @version 1.0
+* @author Youssef Had
+* @package Vinala\Kernel\Cache
+* @since v3.3.0
+*/
+class Item
+{
+	
+	//--------------------------------------------------------
+	// Properties
+	//--------------------------------------------------------
+
+	
+	/**
+	* The symfony cache item
+	*
+	* @var Symfony\Component\Cache\CacheItem 
+	*/
+	protected $item = null ;
+	
+
+	//--------------------------------------------------------
+	// Constructor
+	//--------------------------------------------------------
+
+	function __construct( CacheItem $item)
+	{
+		$this->item = $item ;
+	}
+
+	//--------------------------------------------------------
+	// Functions
+	//--------------------------------------------------------
+
+	/**
+	* Get the value of the item
+	*
+	* @return mixed
+	*/
+	public function value()
+	{
+		return $this->item->get();
+	}
+
+	/**
+	* Get the key name of the item
+	*
+	* @return mixed
+	*/
+	public function name()
+	{
+		return $this->item->getKey();
+	}
+
+	
+	
+
+
+}
