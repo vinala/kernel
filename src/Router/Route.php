2257fdd895ee8f3a1b19281cfce1cd473a612677
youssefhad <youssefhad2@gmail.com>
2017-04-06 21:40:36 +0100

    create new version of route class

--- src/Router/Route.php
+++ src/Router/Route.php
@@ -47,3 +47,75 @@ class Route
 	
 
 }
+
+// namespace Vinala\Kernel\Router ;
+
+/**
+* The callable router surface
+*
+* @version 1.1
+* @author Youssef Had
+* @package Vinala\Kernel\Router
+* @since v3.3.0
+*/
+class Route_
+{
+	//--------------------------------------------------------
+	// Functions
+	//--------------------------------------------------------
+
+	/**
+	* The get function for http get route
+	*
+	* @param string $uri
+	* @param callable $callback
+	* @param string|array $filter
+	* @param array $subdomains
+	* @return mixed
+	*/
+	public static function get($uri , $callback , $filter = null , $subdomains = null )
+	{
+		return Routes::get($uri , $callback , $filter);
+	}
+
+	/**
+	* The post function for HTTP post route
+	* @alpha
+	*
+	* @param string $uri
+	* @param callable $callback
+	* @param string|array $filter
+	* @param array $subdomains
+	* @return mixed
+	*/
+	public static function post($uri , $callback , $filter = null ,$subdomains = null)
+	{
+		return Routes::get($uri , $callback);
+	}
+
+	/**
+	* The controller function for HTTP resource route
+	*
+	* @param string $uri
+	* @param string $controller
+	* @param string|array $filter
+	* @return mixed
+	*/
+	public static function controller($uri , $controller , $filter = null)
+	{
+		return Routes::resource($uri,$controller,$data);
+	}
+
+	/**
+	* The call function for controller function route
+	*
+	* @param string $uri
+	* @param string $controllerFunction
+	* @param string|array $filter
+	* @return mixed
+	*/
+	public static function call($uri , $controllerFunction , $filter = null , $data = null)
+	{
+		return Routes::call($uri , $controllerFunction , $filter , $data);
+	}
+}
