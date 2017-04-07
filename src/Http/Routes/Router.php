553b48715f7a5e84b89575ba041c59ab53fc19e7
hfdgh <dfghdf@dfghdfgh.dfgh>
2017-04-07 11:27:04 +0000

    create new version of Router class

---
+++ src/Http/Routes/Router.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Vinala\Kernel\Http\Routes ;
+
+//use SomeClass;
+
+/**
+* The main router surface
+*
+* @version 3.0
+* @author Youssef Had
+* @package Vinala\Kernel\Http\Routes
+* @since v3.3.0
+*/
+class Router
+{
+    //
+}
