c3578eea7fc67021cbcca7c39d8c837cf54481b8
hfdgh <dfghdf@dfghdfgh.dfgh>
2017-04-11 10:55:14 +0000

    format fetcher

--- src/Foundation/Fetcher.php
+++ src/Foundation/Fetcher.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Vinala\Kernel\Foundation;
 
@@ -14,182 +14,188 @@ use Vinala\Kernel\Environment\Environment;
 */
 class Fetcher
 {
-	/**
-	 * Path to app folder
-	 */
-	protected static $appPath;
+    /**
+     * Path to app folder
+     */
+    protected static $appPath;
 
-	/**
-	 * Path to framework folder
-	 */
-	protected static $frameworkPath;
+    /**
+     * Path to framework folder
+     */
+    protected static $frameworkPath;
 
-	/**
-	 * Get all required App files
-	 */
-	public static function run($routes)
-	{
-		self::setAppPath();
-		self::setFrameworkPath();
-		//
-		self::exception();
-		self::events();
-		self::model();
-		self::controller();
-		self::link();
-		if(Component::isOn("database")) self::seed();
-		self::filtes();
-		self::alias();
-		Environment::ini();
-		//
-		self::routes($routes);
-		self::commands();
-		
+    /**
+     * Get all required App files
+     */
+    public static function run($routes)
+    {
+        self::setAppPath();
+        self::setFrameworkPath();
+        //
+        self::exception();
+        self::events();
+        self::model();
+        self::controller();
+        self::link();
+        if (Component::isOn("database")) {
+            self::seed();
+        }
+        self::filtes();
+        self::alias();
+        Environment::ini();
+        //
+        self::routes($routes);
+        self::commands();
+    }
 
-	}
+    /**
+     * Set path to app folder
+     */
+    protected static function setAppPath()
+    {
+        self::$appPath = Application::$root."app/";
+        return self::$appPath ;
+    }
 
-	/**
-	 * Set path to app folder
-	 */
-	protected static function setAppPath()
-	{
-		self::$appPath = Application::$root."app/";
-		return self::$appPath ; 
-	}
+    /**
+     * Set path to app folder
+     */
+    protected static function setFrameworkPath()
+    {
+        self::$frameworkPath = Application::$root;
+        return self::$frameworkPath ;
+    }
 
-	/**
-	 * Set path to app folder
-	 */
-	protected static function setFrameworkPath()
-	{
-		self::$frameworkPath = Application::$root;
-		return self::$frameworkPath ; 
-	}
+    /**
+     * Fetch files of folder
+     */
+    protected static function fetch($pattern, $app = true)
+    {
+        if ($app) {
+            return glob(self::$appPath.$pattern.'/*.php');
+        } else {
+            return glob(self::$frameworkPath.$pattern.'/*.php');
+        }
+    }
 
-	/**
-	 * Fetch files of folder
-	 */
-	protected static function fetch($pattern,$app = true)
-	{
-		if($app) return glob(self::$appPath.$pattern.'/*.php');
-		else return glob(self::$frameworkPath.$pattern.'/*.php');
-	}
+    /**
+     * Require files of Models folder
+     */
+    protected static function model()
+    {
+        foreach (self::fetch('resources/models', false) as $file) {
+            Bus::need($file);
+        }
+    }
 
-	/**
-	 * Require files of Models folder
-	 */
-	protected static function model()
-	{
-		foreach (self::fetch('resources/models' , false) as $file) 
-			Bus::need($file);
-	}
+    /**
+     * Require files of Controllers folder
+     */
+    protected static function controller()
+    {
+        foreach (self::fetch('resources/controllers', false) as $file) {
+            Bus::need($file);
+        }
+    }
 
-	/**
-	 * Require files of Controllers folder
-	 */
-	protected static function controller()
-	{
-		foreach (self::fetch('resources/controllers' , false) as $file) 
-			Bus::need($file);
-	}
+    /**
+     * Require files of Links folder
+     */
+    protected static function link()
+    {
+        foreach (self::fetch("links") as $file) {
+            Bus::need($file);
+        }
+    }
 
-	/**
-	 * Require files of Links folder
-	 */
-	protected static function link()
-	{
-		foreach (self::fetch("links") as $file) 
-			Bus::need($file);
-	}
+    /**
+     * Require files of exception folder
+     */
+    protected static function exception()
+    {
+        foreach (self::fetch("exceptions") as $file) {
+            Bus::need($file);
+        }
+    }
 
-	/**
-	 * Require files of exception folder
-	 */
-	protected static function exception()
-	{
-		foreach (self::fetch("exceptions") as $file) 
-			Bus::need($file);
-	}
+    /**
+     * Require files of Seeds folder
+     */
+    protected static function seed()
+    {
+        foreach (self::fetch("database/seeds", false) as $file) {
+            Bus::need($file);
+        }
+    }
 
-	/**
-	 * Require files of Seeds folder
-	 */
-	protected static function seed()
-	{
-		foreach (self::fetch("database/seeds" , false) as $file) 
-			Bus::need($file);
-	}
+    /**
+     * Require files of Filters
+     *
+     * @deprecated 3.3.0
+     */
+    protected static function filtes()
+    {
+        /**
+        * The filters fetches are transported
+        * to router Bus calls
+        **/
+    }
 
-	/**
-	 * Require files of Filters
-	 *
-	 * @deprecated 3.3.0
-	 */
-	protected static function filtes()
-	{
-		/**
-		* The filters fetches are transported
-		* to router Bus calls
-		**/
-	}
+    /**
+     * set app aliases
+     */
+    protected static function alias()
+    {
+        return Alias::appAlias();
+    }
 
-	/**
-	 * set app aliases
-	 */
-	protected static function alias()
-	{
-		return Alias::appAlias();
-	}
+    /**
+    * Require and call middleware classes
+    *
+    * @return null
+    */
+    protected static function middleware()
+    {
+        foreach (self::fetch("http/middleware") as $file) {
+            Bus::need($file);
+        }
+                    
+        Middleware::ini();
+    }
+    
 
-	/**
-	* Require and call middleware classes
-	*
-	* @return null
-	*/
-	protected static function middleware()
-	{
-		foreach (self::fetch("http/middleware") as $file) 
-		{
-			Bus::need($file);
-		}
-					
-		Middleware::ini();
-	}
-	
+    /**
+     * Require files of Routes
+     */
+    protected static function routes($routes)
+    {
+        if ($routes) {
+                self::middleware();
 
-	/**
-	 * Require files of Routes
-	 */
-	protected static function routes($routes)
-	{
-		if($routes)
-			{
-				self::middleware();
+                Bus::need(self::$appPath.'http/Routes.php');
+                Routes::run();
+        }
+    }
 
-				Bus::need(self::$appPath.'http/Routes.php');
-				Routes::run();
-				// \Vinala\Kernel\Http\Router\Routes::run();
-			}
-	}
+    /**
+     * Require files of Console
+     */
+    protected static function commands()
+    {
+        foreach (self::fetch("support/shell", false) as $file) {
+            Bus::need($file);
+        }
+    }
 
-	/**
-	 * Require files of Console
-	 */
-	protected static function commands()
-	{
-		foreach (self::fetch("support/shell" , false) as $file) 
-			Bus::need($file);
-	}
-
-	/**
-	 * Require files of Console
-	 */
-	protected static function events()
-	{
-		foreach (self::fetch("app/events" , false) as $file) 
-			Bus::need($file);
-		//
-		Event::register();
-	}
-	
+    /**
+     * Require files of Console
+     */
+    protected static function events()
+    {
+        foreach (self::fetch("app/events", false) as $file) {
+            Bus::need($file);
+        }
+        //
+        Event::register();
+    }
 }
