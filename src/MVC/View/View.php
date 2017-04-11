2529ac00c92f20587cb26d4a14eed53247bef770
hfdgh <dfghdf@dfghdfgh.dfgh>
2017-04-11 10:54:17 +0000

    format view class

--- src/MVC/View/View.php
+++ src/MVC/View/View.php
@@ -3,97 +3,88 @@
 namespace Vinala\Kernel\MVC;
 
 use Vinala\Kernel\MVC\Views;
+
 /**
 * View mother class
 */
 class View
 {
 
-	//--------------------------------------------------------
-	// Properties
-	//--------------------------------------------------------
+    //--------------------------------------------------------
+    // Properties
+    //--------------------------------------------------------
 
 
-	/**
-	* The last view used by makefunction
-	*
-	* @var Vinala\Kernel\MVC\Views 
-	*/
-	protected static $view ;
-	
+    /**
+    * The last view used by makefunction
+    *
+    * @var Vinala\Kernel\MVC\Views
+    */
+    protected static $view ;
+    
 
-	//--------------------------------------------------------
-	// Functions
-	//--------------------------------------------------------
+    //--------------------------------------------------------
+    // Functions
+    //--------------------------------------------------------
 
-	/**
-	* Call and make a view
-	*
-	* @param string $name
-	* @param array $data
-	* @return Vinala\Kernel\MVC\Views
-	*/
-	public static function make( $name , $data = null , $nest = null)
-	{
-		self::$view = new Views;
+    /**
+    * Call and make a view
+    *
+    * @param string $name
+    * @param array $data
+    * @return Vinala\Kernel\MVC\Views
+    */
+    public static function make($name, $data = null, $nest = null)
+    {
+        self::$view = new Views;
 
-		return self::$view->call($name , $data , $nest);
-	}
+        return self::$view->call($name, $data, $nest);
+    }
 
-	/**
-	* Show a view
-	*
-	* @param Vinala\Kernel\MVC\Views $view
-	* @return null
-	*/
-	public static function show(Views $view)
-	{
-		return $view->show($view);
-	}
+    /**
+    * Show a view
+    *
+    * @param Vinala\Kernel\MVC\Views $view
+    * @return null
+    */
+    public static function show(Views $view)
+    {
+        return $view->show($view);
+    }
 
-	/**
-	* Extend to child view
-	*
-	* @param string $name
-	* @param array $data
-	* @return 
-	*/
-	public static function extend($name , $data = null)
-	{
-		return self::$view->call($name , $data)->show();
-	}
-	
-	
-	
-
+    /**
+    * Extend to child view
+    *
+    * @param string $name
+    * @param array $data
+    * @return null
+    */
+    public static function extend($name, $data = null)
+    {
+        return self::$view->call($name, $data)->show();
+    }
+    
+    //------------------------------
 
 
+    public static function get($value, $data = null)
+    {
+        return Views::get($value, $data);
+    }
 
+    public static function import($plugin, $value, $data = null)
+    {
+        return Views::import($plugin, $value, $data);
+    }
 
-	public static function get($value,$data=null)
-	{
-		return Views::get($value,$data);
-	}
-
-	public static function import($plugin,$value,$data=null)
-	{
-		return Views::import($plugin,$value,$data);
-	}
-
-	/**
-	* Check if the view exists
-	*
-	* @param string $name
-	* @return bool
-	*/
-	public static function exists($name , $nest = null)
-	{
-		return instance(Views::class)->exists($name, $nest);
-	}
-
-
-	
-	
-
-
+    /**
+    * Check if the view exists
+    *
+    * @param string $name
+    * @return bool
+    */
+    public static function exists($name, $nest = null)
+    {
+        return instance(Views::class)->exists($name, $nest);
+    }
 }
