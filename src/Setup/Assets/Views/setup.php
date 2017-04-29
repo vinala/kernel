814c333ff0c0609878f4b9abfba32fc01c8fe828
youssefhad <youssefhad2@gmail.com>
2017-04-29 14:23:18 +0100

    use the change log in master branch instead of dev branch

--- src/Setup/Assets/Views/setup.php
+++ src/Setup/Assets/Views/setup.php
@@ -107,7 +107,7 @@
 		</div>
 		
 		<div class="final_link_version" id="bottom_panel_2" style="display:none">
-			<a id="fst_panel" class="final_link_owner_a" href="https://github.com/vinala/vinala/blob/dev/CHANGES.md">
+			<a id="fst_panel" class="final_link_owner_a" href="https://github.com/vinala/vinala/blob/master/CHANGES.md">
 				<?php echo "v".App::getVersion()->full(); ?>
 			</a>
 		</div>	
