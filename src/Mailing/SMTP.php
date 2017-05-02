7693364818a657038bbe25b436f45e21ef2c8c83
Youssef <youssefhad2@gmail.com>
2017-05-02 12:50:52 +0000

    update SMTP class

---
+++ src/Mailing/SMTP.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Vinala\Kernel\Mailing ;
+
+//use SomeClass;
+
+/**
+* The SMTP protocol class
+*
+* @version 1.0
+* @author Youssef Had
+* @package Vinala\Kernel\Mailing
+* @since v3.3.0
+*/
+class SMTP
+{
+
+    //--------------------------------------------------------
+    // Properties
+    //--------------------------------------------------------
+
+    /**
+    * The SMTP host
+    *
+    * @var string
+    */
+    private $host ;
+
+    
+    /**
+    * The SMTP port
+    *
+    * @var int
+    */
+    private $port ;
+
+
+    /**
+    * The SMTP encryption
+    *
+    * @var string
+    */
+    private $encryption ;
+
+
+    /**
+    * The SMTP username
+    *
+    * @var string
+    */
+    private $username;
+
+
+    /**
+    * The SMTP password
+    *
+    * @var string
+    */
+    private $password ;
+
+
+    /**
+    * The SMTP from credits
+    *
+    * @var array
+    */
+    private $from;
+
+
+
+    //--------------------------------------------------------
+    // Constructor
+    //--------------------------------------------------------
+
+    function __construct($host, $port, $secure, $username, $password, $from)
+    {
+        $this->host = $host ;
+        $this->port = $port ;
+        $this->secure = $secure ;
+        $this->username = $username ;
+        $this->password = $password ;
+        $this->from = $from ;
+    }
+
+    //--------------------------------------------------------
+    // Functions
+    //--------------------------------------------------------
+
+    //
+}
