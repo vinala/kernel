389a56251c9cf9cfbf5fd338aa6d0877a48f0a9c
Youssef <youssefhad2@gmail.com>
2017-05-11 10:25:57 +0000

    rename router processor

--- src/Foundation/Bus.php
+++ src/Foundation/Bus.php
@@ -1109,7 +1109,7 @@ class Bus
      */
     private static function proccess()
     {
-        $files = ['Process', 'Command', 'Translator', 'Schema', 'Links', 'Tag', 'Model', 'View', 'Controller', 'Seeds', 'Routes', 'Exception', 'Events', 'Alias', 'Middleware', 'Helper', 'Tests'];
+        $files = ['Process', 'Command', 'Translator', 'Schema', 'Links', 'Tag', 'Model', 'View', 'Controller', 'Seeds', 'Router', 'Exception', 'Events', 'Alias', 'Middleware', 'Helper', 'Tests'];
         $folder = static::$root.'Processes'.'/';
 
         self::call($files, $folder);
