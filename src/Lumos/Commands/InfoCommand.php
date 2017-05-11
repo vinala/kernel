b1170d62d6b3f2ba4393ae3ca6bb342585057190
Youssef <youssefhad2@gmail.com>
2017-05-11 15:11:58 +0000

    update info command

--- src/Lumos/Commands/InfoCommand.php
+++ src/Lumos/Commands/InfoCommand.php
@@ -42,15 +42,15 @@ class InfoCommand extends Commands
         if (!empty(config('app.owner'))) {
             $this->line('by '.config('app.owner'));
         }
-        $this->line("\n\n");
+        $this->line("");
         $this->line('***********');
         $this->line('Based on : ');
         $version = Application::getVersion()->console();
         $this->question('Vinala ', true);
-        $this->line("v$version PHP Framework");
-        $this->line("\n");
+        $this->line("Framework v$version");
+        $this->line("-------");
         $this->line(Application::getVersion()->kernel());
-        $this->line("\n");
+        $this->line("-------");
         $this->write('created by Youssef Had (');
         $this->question('youssefhad2@gmail.com - www.facebook.com/yussef.had', true);
         $this->line(')');
