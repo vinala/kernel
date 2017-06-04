6295aaed5ecc1636dbbf231862b58bf067dbe369
Youssef <youssefhad2@gmail.com>
2017-06-04 18:33:20 +0000

    create querying class

---
+++ src/Database/Query/Querying.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Vinala\Kernel\Database\Query ;
+
+//use SomeClass;
+
+/**
+ * Class to make queries more flexible
+ *
+ * @version 1.0
+ * @author Youssef Had
+ * @package Vinala\Kernel\Database\Query
+ * @since v3.3.0
+ */
+class Querying
+{
+
+    //--------------------------------------------------------
+    // Properties
+    //--------------------------------------------------------
+
+    /**
+     * The Query class
+     *
+     * @var Vinala\Kernel\Database\Query
+     */
+    private $query ;
+
+    //--------------------------------------------------------
+    // Constructor
+    //--------------------------------------------------------
+
+    function __construct()
+    {
+        //
+    }
+
+    //--------------------------------------------------------
+    // Functions
+    //--------------------------------------------------------
+
+    //
+}
